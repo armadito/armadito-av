c744e0d0ae53f483c8f01f193165dd6442ec8bb7
François Déchelle <francois@dechelle.net>
2014-07-17 11:54:40 +0200

    add src and test

---
+++ libumwsu/src/module.c
@@ -0,0 +1,71 @@
+#include <libumwsu/umwsu.h>
+
+#include <alloca.h>
+#include <assert.h>
+#include <gmodule.h>
+#include <stdio.h>
+#include <string.h>
+
+static void module_name_from_path(const char *path, char *module_name, size_t n)
+{
+  char *start, *end;
+
+  start = strrchr(path, '/');
+  assert(start != NULL);
+
+  end = strrchr(path, '.');
+  assert(end != NULL);
+
+  assert(end > start);
+
+  assert(end - start < n -1);
+
+  n = end - start;
+  strncpy(module_name, start + 1, n);
+  module_name[n - 1] = '\0';
+}
+
+int umw_module_install(const char *path)
+{
+  GModule *module;
+#define MODULE_NAME_LEN 128
+  char module_name[MODULE_NAME_LEN];
+  char *install_fun_name;
+  void (*install_fun)(void);
+
+  module = g_module_open(path, G_MODULE_BIND_LAZY);
+
+  if (!module) {
+    /* g_set_error(error, FOO_ERROR, FOO_ERROR_BLAH,  "%s", g_module_error()); */
+    fprintf(stderr, "unable to open %s\n", path);
+    return FALSE;
+  }
+
+  module_name_from_path(path, module_name, MODULE_NAME_LEN);
+
+  install_fun_name = (char *)alloca(MODULE_NAME_LEN + 8 + 1);
+  snprintf(install_fun_name, MODULE_NAME_LEN + 8 + 1, "%s_install", module_name);
+
+  if (!g_module_symbol(module, install_fun_name, (gpointer *)&install_fun)) {
+    /* g_set_error(error, SAY_ERROR, SAY_ERROR_OPEN, "%s: %s", path, g_module_error()); */
+    fprintf(stderr, "cannot find symbol %s\n", install_fun_name);
+    if (!g_module_close(module))
+      g_warning("%s: %s", path, g_module_error());
+    return FALSE;
+  }
+
+  if (install_fun == NULL) {
+    /* g_set_error(error, SAY_ERROR, SAY_ERROR_OPEN, "install function is NULL"); */
+    fprintf(stderr, "install function is NULL\n");
+    if(!g_module_close(module))
+      g_warning("%s: %s", path, g_module_error());
+    return FALSE;
+  }
+
+  (*install_fun)();
+
+  if(!g_module_close(module))
+    g_warning("%s: %s", path, g_module_error());
+
+  return TRUE;
+}
---
+++ libumwsu/tests/module1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void module1_install(void)
+{
+  fprintf(stderr, "module1 installed ok\n");
+}
