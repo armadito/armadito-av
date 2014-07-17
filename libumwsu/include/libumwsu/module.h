89df08ccd4bc9c8e6ad740177087e5efd30eab48
François Déchelle <francois@dechelle.net>
2014-07-17 11:54:52 +0200

    add src and test

--- libumwsu/include/libumwsu/module.h
+++ libumwsu/include/libumwsu/module.h
@@ -1,15 +1,24 @@
 #ifndef _LIBUMWSU_MODULE_H_
 #define _LIBUMWSU_MODULE_H_
 
-enum uw_mod_status {
+#if 0
+enum umw_scan_status;
+
+enum umw_mod_status {
   UMW_MOD_OK,
   UMW_MOD_INIT_ERROR,
   UMW_MOD_CONF_ERROR,
 };
 
-typedef enum uw_mod_status (umw_fun_init_t)(void **pdata);
-typedef int (umw_fun_conf_t)(const char *key, const char *value, void *data);
+struct umw_module {
+  enum umw_mod_status (*conf_init)(void **pmod_conf_data);
+  enum umw_mod_status (*conf)(const char *key, const char *value, void *mod_conf_data);
+  enum umw_mod_status (*init)(void **pmod_data);
+  enum umw_scan_status (*scan)(const char *path, void *mod_data);
+  enum umw_mod_status (*close)(void *mod_data);
+};
+#endif
 
-void umw_module_install(void);
+int umw_module_install(const char *path);
 
 #endif
--- libumwsu/include/libumwsu/scan.h
+++ libumwsu/include/libumwsu/scan.h
@@ -1,11 +1,15 @@
 #ifndef _LIBUMWSU_SCAN_H_
 #define _LIBUMWSU_SCAN_H_
 
-enum uw_status {
+enum umw_status {
   UMW_CLEAN,
   UMW_INFECTED,
   UMW_EINVAL,
   UMW_IERROR,
 };
+
+enum umw_status umw_scan_file(const char *path);
+
+enum umw_status umw_scan_dir(const char *path, int recurse);
 
 #endif
