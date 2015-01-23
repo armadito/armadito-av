c09b523e41922593774de862d85b3a6053a07d4e
François Déchelle <francois@dechelle.net>
2015-01-23 17:20:44 +0100

    * added summary print and better option parsing to uhuru-scan CLI

--- libumwsu/tools/uhuru-scan.c
+++ libumwsu/tools/uhuru-scan.c
@@ -2,52 +2,156 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
-void report_print_callback(struct umwsu_report *report, void *callback_data)
+struct umwsu_scan_summary {
+  int scanned;
+  int malware;
+  int suspicious;
+  int unhandled;
+  int clean;
+};
+
+struct umwsu_scan_options {
+  int recursive;
+  int threaded;
+  int no_summary;
+  /* more options later */
+  char *file_or_dir;
+};
+
+static struct option long_options[] = {
+  {"help",         no_argument,       0, 'h'},
+  {"recursive",    no_argument,       0, 'r'},
+  {"threaded",     no_argument,       0, 't'},
+  {"no-summary",   no_argument,       0, 'n'},
+  {0, 0, 0, 0}
+};
+
+static void usage(void)
+{
+  fprintf(stderr, "usage: uhuru-scan [options] FILE|DIR\n");
+  fprintf(stderr, "\n");
+  fprintf(stderr, "Uhuru antivirus scanner\n");
+  fprintf(stderr, "\n");
+  fprintf(stderr, "Options:\n");
+  fprintf(stderr, "  --help  -h               print help and quit\n");
+  fprintf(stderr, "  --recursive  -r          scan directories recursively\n");
+  fprintf(stderr, "  --threaded -t            scan using multiple threads\n");
+  fprintf(stderr, "  --no-summary -n          disable summary at end of scanning\n");
+  fprintf(stderr, "\n");
+
+  exit(1);
+}
+
+static void parse_options(int argc, char *argv[], struct umwsu_scan_options *opts)
+{
+  int c;
+
+  opts->recursive = 0;
+  opts->threaded = 1;
+  opts->no_summary = 0;
+  opts->file_or_dir = NULL;
+
+  while (1) {
+    int option_index = 0;
+
+    c = getopt_long(argc, argv, "hrtn", long_options, &option_index);
+
+    if (c == -1)
+      break;
+
+    switch (c) {
+    case 'h':
+      usage();
+      break;
+    case 'r':
+      opts->recursive = 1;
+      break;
+    case 't':
+      opts->threaded = 1;
+      break;
+    case 'n':
+      opts->no_summary = 1;
+      break;
+    default:
+      usage();
+    }
+  }
+
+  if (optind < argc)
+    opts->file_or_dir = argv[optind];
+
+  if (opts->file_or_dir == NULL)
+    usage();
+}
+
+static void report_print_callback(struct umwsu_report *report, void *callback_data)
 {
   FILE *out = (FILE *)callback_data;
 
   umwsu_report_print(report, out);
 }
 
-static void usage(int argc, char **argv)
+static void summary_callback(struct umwsu_report *report, void *callback_data)
 {
-  fprintf(stderr, "usage: %s [-r] FILE|DIR ...\n", argv[0]);
-  exit(1);
+  struct umwsu_scan_summary *s = (struct umwsu_scan_summary *)callback_data;
+
+  s->scanned++;
+
+  switch(report->status) {
+  case UMWSU_MALWARE:
+    s->malware++;
+    break;
+  case UMWSU_SUSPICIOUS:
+    s->suspicious++;
+    break;
+  case UMWSU_EINVAL:
+  case UMWSU_IERROR:
+  case UMWSU_UNKNOWN_FILE_TYPE:
+  case UMWSU_UNDECIDED:
+    s->unhandled++;
+    break;
+  case UMWSU_WHITE_LISTED:
+  case UMWSU_CLEAN:
+    s->clean++;
+    break;
+  }
 }
 
 int main(int argc, char **argv)
 {
   struct umwsu *u;
   struct umwsu_scan *scan;
-  int argp = 1;
   enum umwsu_scan_flags flags = 0;
+  struct umwsu_scan_options opts;
+  struct umwsu_scan_summary summary;
   
-  if (argc < 2)
-    usage(argc, argv);
+  parse_options(argc, argv, &opts);
 
-  if (!strcmp(argv[argp], "-r")) {
-    argp++;
+  if (opts.recursive)
     flags |= UMWSU_SCAN_RECURSE;
-  }
 
-  if (!strcmp(argv[argp], "-t")) {
-    argp++;
+  if (opts.threaded)
     flags |= UMWSU_SCAN_THREADED;
-  }
-
-  if (argp >= argc)
-    usage(argc, argv);
 
   u = umwsu_open();
 
   umwsu_set_verbose(u, 1);
 
+#if 0
   umwsu_print(u);
+#endif
 
-  scan = umwsu_scan_new(u, argv[argp], flags);
+  scan = umwsu_scan_new(u, opts.file_or_dir, flags);
 
   umwsu_scan_add_callback(scan, report_print_callback, stdout);
+
+  if (!opts.no_summary) {
+    summary.scanned = summary.malware = summary.suspicious = summary.unhandled = summary.clean = 0;
+
+    umwsu_scan_add_callback(scan, summary_callback, &summary);
+  }
 
   umwsu_scan_start(scan);
 
@@ -56,6 +160,15 @@ int main(int argc, char **argv)
   umwsu_scan_free(scan);
 
   umwsu_close(u);
+
+  if (!opts.no_summary) {
+    printf("\nSCAN SUMMARY:\n");
+    printf("scanned files     : %d\n", summary.scanned);
+    printf("malware files     : %d\n", summary.malware);
+    printf("suspicious files  : %d\n", summary.suspicious);
+    printf("unhandled files   : %d\n", summary.unhandled);
+    printf("clean files       : %d\n", summary.clean);
+  }
 
   return 0;
 }
