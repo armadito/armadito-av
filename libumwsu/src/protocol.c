53a479d59d7cc972d65998f3525e6141cd3376f3
François Déchelle <francois@dechelle.net>
2015-02-10 17:44:50 +0100

    * implemented first working version of protocol handler

--- libumwsu/src/protocol.c
+++ libumwsu/src/protocol.c
@@ -1,8 +1,7 @@
 #include "protocol.h"
 
-#include "protocol.h"
-
 #include <glib.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 struct protocol_command {
@@ -10,67 +9,131 @@ struct protocol_command {
   GHashTable *headers;
 };
 
-enum protocol_parser_state {
-  E_COMMAND,
+static void str_free(gpointer p)
+{
+  g_free(p);
+}
+
+static struct protocol_command *protocol_command_new(void)
+{
+  struct protocol_command *cmd;
+
+  cmd = (struct protocol_command *)malloc(sizeof(struct protocol_command));
+  cmd->command = g_string_new("");
+  cmd->headers = g_hash_table_new_full(g_str_hash, g_str_equal, str_free, str_free);
+
+  return cmd;
+}
+
+char *protocol_command_cmd(struct protocol_command *cmd)
+{
+  return cmd->command->str;
+}
+
+char *protocol_command_header(struct protocol_command *cmd, const char *key)
+{
+  return (char *)g_hash_table_lookup(cmd->headers, key);
+}
+
+
+enum protocol_handler_state {
+  EXPECTING_COMMAND,
   IN_COMMAND,
-  E_HEADER,
+  EXPECTING_HEADER,
   IN_HEADER,
-  E_VALUE,
+  EXPECTING_VALUE,
   IN_VALUE,
 };
 
-struct protocol_parser {
-  enum protocol_parser_state state;
+struct protocol_handler {
+  enum protocol_handler_state state;
+  GString *current_header_key, *current_header_value;
+  struct protocol_command *cmd;
 };
+
+struct protocol_handler *protocol_handler_new(void)
+{
+  struct protocol_handler *h;
+
+  h = (struct protocol_handler *)malloc(sizeof(struct protocol_handler));
+
+  h->state = EXPECTING_COMMAND;
+  h->current_header_key = g_string_new("");
+  h->current_header_value = g_string_new("");
+  h->cmd = protocol_command_new();
+
+  return h;
+}
+
+static void protocol_error(struct protocol_handler *h, char c)
+{
+  fprintf(stderr, "Error in protocol: state %d char %c\n", h->state, c);
+}
+
+static void protocol_handler_end_of_header(struct protocol_handler *h)
+{
+  g_hash_table_insert(h->cmd->headers, g_strdup(h->current_header_key->str), g_strdup(h->current_header_value->str));
+  g_string_truncate(h->current_header_key, 0);
+  g_string_truncate(h->current_header_value, 0);
+}
+
+static void GH_print_func(gpointer key, gpointer value, gpointer user_data)
+{
+  fprintf(stderr, "Header: key=%s value=\"%s\"\n", (char *)key, (char *)value);
+}
+
+static void protocol_handler_end_of_command(struct protocol_handler *h)
+{
+  fprintf(stderr, "Command: %s\n", h->cmd->command->str);
+  g_hash_table_foreach (h->cmd->headers, GH_print_func, NULL);
+  fprintf(stderr, "\n");
+
+  g_string_truncate(h->cmd->command, 0);
+  g_hash_table_remove_all(h->cmd->headers);
+  g_string_truncate(h->current_header_key, 0);
+  g_string_truncate(h->current_header_value, 0);
+}
 
 #define STAY(A) (A)
 #define MOVE(A,S) do { A; state = S; } while (0)
 
-#if 0
-void protocol_decode(struct protocol_hander *h)
+int protocol_handler_input_char(struct protocol_handler *h, char c)
 {
-  switch(state) {
-  case IN_COMMAND:
-    if (isalpha(c))
-      append_to_command(c);
-    else if (c == '\n') {
-      append_to_command('\0');
-      state = IN_HEADER_NAME;
-    } else
-      protocol_error(c);
-    break;
-  case IN_HEADER_NAME:
-    if (isalpha(c))
-      append_to_header_name(c);
-    else if (c == ':') {
-      append_to_header_name('\0');
-      state = IN_HEADER_VALUE_SKIP_SPACE;
-    } else if (c == '\n') {
-      end_of_command();   /* includes reset_headers(); */
-      state = IN_COMMAND;
-    } else
-      protocol_error(c);
-    break;
-  case IN_HEADER_VALUE_SKIP_SPACE:
-    if (isalpha(c)) {
-      append_to_header_value(c);
-      state = IN_HEADER_VALUE;
-    } else if (is_space(c))
-      ;
+  switch(h->state) {
+  case EXPECTING_COMMAND:
+    if (isupper(c) || c == '_')
+      g_string_append_c(h->cmd->command, c);
+    else if (c == '\n')
+      h->state = EXPECTING_HEADER;
     else
-      protocol_error(c);
+      protocol_error(h, c);
     break;
-  case IN_HEADER_VALUE:
-    if (isalpha(c)) {
-      append_to_header_value(c);
-    } else if (c == '\n') {
-      append_to_header_value('\0');
-      next_header();
-      state = IN_HEADER_NAME;
+  case EXPECTING_HEADER:
+    if (c == '\n') {
+      protocol_handler_end_of_command(h);
+      h->state = EXPECTING_COMMAND;
+    } else if (isalnum(c) || c == '-' || c == '_')
+      g_string_append_c(h->current_header_key, c);
+    else if (c == ':') {
+      h->state = EXPECTING_VALUE;
     } else
-      protocol_error(c);
+      protocol_error(h, c);
+    break;
+  case EXPECTING_VALUE:
+    if (!isblank(c)) {
+      g_string_append_c(h->current_header_value, c);
+      h->state = IN_VALUE;
+    }
+    break;
+  case IN_VALUE:
+    if (c == '\n') {
+      protocol_handler_end_of_header(h);
+      h->state = EXPECTING_HEADER;
+    } else
+      g_string_append_c(h->current_header_value, c);
     break;
   }
 
+  return 0;
 }
-#endif
+
--- libumwsu/src/protocol.h
+++ libumwsu/src/protocol.h
@@ -8,12 +8,12 @@ char *protocol_command_header(struct protocol_command *cmd, const char *key);
 
 struct protocol_handler;
 
-struct protocol_handler *protocol_handler_new(int fd);
-
 typedef void (*protocol_handler_cb_t)(struct protocol_command *cmd, void *data);
+
+struct protocol_handler *protocol_handler_new(void);
 
 int protocol_handler_add_callback(struct protocol_handler *handler, protocol_handler_cb_t cb, void *data);
 
-int protocol_handler_input(void);
+int protocol_handler_input_char(struct protocol_handler *handler, char c);
 
 #endif
