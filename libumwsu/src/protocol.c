9a57bb1abe6dafdd460cd8dc6eed381817609d00
François Déchelle <francois@dechelle.net>
2015-02-10 15:09:05 +0100

    * adding scan process protocol implementation

--- libumwsu/src/protocol.c
+++ libumwsu/src/protocol.c
@@ -1,105 +1,32 @@
 #include "protocol.h"
 
+#include "protocol.h"
+
+#include <glib.h>
 #include <stdlib.h>
 
-#define PROTOCOL_BUFFER_INITIAL_SIZE 32
-
-struct buffer {
-  char *head;
-  char *tail;
-  int alloc;
+struct protocol_command {
+  GString *command;
+  GHashTable *headers;
 };
 
-void buffer_init(struct protocol_buffer *b)
-{
-  b->alloc = PROTOCOL_BUFFER_INITIAL_SIZE;
-  b->head = (char *)malloc(b->alloc);
-  b->tail = b->head;
-}
-
-void protocol_buffer_destroy(struct protocol_buffer *b)
-{
-  free(b->buff);
-}
-
-void protocol_buffer_grow(struct protocol_buffer *b, int needed)
-{
-  if (b->head + needed >= b->alloc) {
-    while(b->head + needed >= b->alloc)
-      b->alloc *= 2;
-    b->buff = (char *)realloc(b->buff, b->alloc);
-  }
-}
-
-void protocol_buffer_append_v(struct protocol_buffer *b, char *p)
-{
-  while(*p) {
-    switch(*p) {
-    case '\n':
-      protocol_buffer_grow(b, 2);
-      b->buff[b->head++] = '\\';
-      b->buff[b->head++] = 'n';
-      break;
-    default:
-      protocol_buffer_grow(b, 1);
-      b->buff[b->head++] = *p;
-      break;
-    }
-
-    p++;
-  }
-}
-
-void protocol_buffer_append_s(struct protocol_buffer *b, char *p, int size)
-{
-  int i;
-
-  protocol_buffer_grow(b, size);
-  
-  for(i = 0; i < size; i++)
-    b->buff[b->head++] = *p++;
-}
-
-struct header {
-  struct vector name;
-  struct vector value;
+enum protocol_parser_state {
+  E_COMMAND,
+  IN_COMMAND,
+  E_HEADER,
+  IN_HEADER,
+  E_VALUE,
+  IN_VALUE,
 };
 
-struct command {
-  struct vector name;
-  struct vector headers;
+struct protocol_parser {
+  enum protocol_parser_state state;
 };
-
-void command_init(struct command *c)
-{
-  
-}
-
-void command_append_name(struct command *c, char p)
-{
-}
-
-void header_append_name(struct header *h, char *p)
-{
-}
-
-void header_append_name(struct header *h, char *p)
-{
-}
-
-char *header_name(struct header *h)
-{
-  return NULL;
-}
-
-char *header_value(struct header *h)
-{
-  return NULL;
-}
 
 #define STAY(A) (A)
 #define MOVE(A,S) do { A; state = S; } while (0)
 
+#if 0
 void protocol_decode(struct protocol_hander *h)
 {
   switch(state) {
@@ -146,3 +73,4 @@ void protocol_decode(struct protocol_hander *h)
   }
 
 }
+#endif
--- libumwsu/src/protocol.h
+++ libumwsu/src/protocol.h
@@ -1,53 +1,19 @@
 #ifndef _PROTOCOL_H_
 #define _PROTOCOL_H_
 
-/*
-  Command: PING
-  Headers: none
-  Response: PONG
-  Response headers: none
-  Connexion: close
+struct protocol_command;
 
-  Command: SCAN
-  Headers:
-    Path: <path_to_scan>
-  Response: SCANNED
-  Response headers:
-    Path: <scanned_path> (is a file)
-    Status: OK | UNDECIDED | UNKNOWN | EINVAL | IERROR | SUSPICIOUS | MALWARE
-    X-Status: <specific to scan module> (for instance virus name) (optional)
-    Action: CLEAN | QUARANTINE | REMOVED | NONE | ...
-  Repeat response: yes
-  Connexion: not closed
-  Response: SCAN_END
-  Response headers: none
-  Connexion: close
+char *protocol_command_cmd(struct protocol_command *cmd);
+char *protocol_command_header(struct protocol_command *cmd, const char *key);
 
-  Command: STATS
-  Headers: none
-  Response: STATS
-  Response headers:
-    Stat: line
-    Repeat header: yes
-  Connexion: close
-  
-later:
-  Command: WATCH
-  Headers:
-    Path: <path_to_watch>
-  Response: SCANNED
-  idem SCAN, but no END response
+struct protocol_handler;
 
+struct protocol_handler *protocol_handler_new(int fd);
 
-Clamd examples:
-$ echo "nVERSION" | netcat -U /var/run/clamav/clamd.ctl
-ClamAV 0.98.1/19684/Wed Nov 26 15:29:58 2014
-$ echo "nVERSIONCOMMANDS" | netcat -U /var/run/clamav/clamd.ctl
-ClamAV 0.98.1/19684/Wed Nov 26 15:29:58 2014| COMMANDS: SCAN QUIT RELOAD PING CONTSCAN VERSIONCOMMANDS VERSION STREAM END SHUTDOWN MULTISCAN FILDES STATS IDSESSION INSTREAM DETSTATSCLEAR DETSTATS ALLMATCHSCAN
+typedef void (*protocol_handler_cb_t)(struct protocol_command *cmd, void *data);
 
-*/
+int protocol_handler_add_callback(struct protocol_handler *handler, protocol_handler_cb_t cb, void *data);
 
-
-
+int protocol_handler_input(void);
 
 #endif
