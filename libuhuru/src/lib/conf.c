f4323ef02eb7bdf59489f27a8822d3c18026d4b9
François Déchelle <francois@dechelle.net>
2015-08-24 10:50:26 +0200

    * changed configuration file parser to use its own scanner; using glib scanner was painfull because of distinction integer/strings; now all integers are returned as strings; tests ok

--- libuhuru/src/lib/conf.c
+++ libuhuru/src/lib/conf.c
@@ -7,42 +7,166 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <setjmp.h>
-#include <fcntl.h>
+#include <ctype.h>
 
-
-static GScannerConfig conf_parser_config = {
-  .cset_skip_characters = " \t\n",
-  .cset_identifier_first = G_CSET_a_2_z "_" G_CSET_A_2_Z,
-  .cset_identifier_nth = G_CSET_a_2_z "_0123456789" G_CSET_A_2_Z G_CSET_LATINS G_CSET_LATINC "-",
-  .cpair_comment_single = "#\n",
-  .case_sensitive = TRUE,		/* Should symbol lookup work case sensitive? */
-  .skip_comment_multi = TRUE,		/* C like comment */
-  .skip_comment_single = TRUE,		/* single line comment */
-  .scan_comment_multi = TRUE,		/* scan multi line comments? */
-  .scan_identifier = TRUE,
-  .scan_identifier_1char = TRUE,
-  .scan_identifier_NULL = FALSE,
-  .scan_symbols = TRUE,
-  .scan_binary = FALSE,
-  .scan_octal = TRUE,
-  .scan_float = FALSE,
-  .scan_hex = TRUE,			/* '0x0ff0' */
-  .scan_hex_dollar = FALSE,		/* '$0ff0' */
-  .scan_string_sq = FALSE,		/* string: 'anything' */
-  .scan_string_dq = TRUE,		/* string: "\\-escapes!\n" */
-  .numbers_2_int = TRUE,		/* bin, octal, hex => int */
-  .int_2_float = FALSE,			/* int => G_TOKEN_FLOAT? */
-  .identifier_2_string = TRUE,		/* identifier returned as strings */
-  .char_2_token = TRUE,
-  .symbol_2_token = FALSE,
-  .scope_0_fallback = FALSE,
-  .store_int64 = FALSE,   
+struct scanner {
+  int cur_line;
+  int cur_position;
+  FILE *input;
+  GString *token_text;
 };
 
+enum token_type {
+  TOKEN_EOF			=   0,
+  TOKEN_LEFT_BRACE		= '[',
+  TOKEN_RIGHT_BRACE		= ']',
+  TOKEN_EQUAL_SIGN		= '=',
+  TOKEN_COMMA			= ',',
+  TOKEN_SEMI_COLON		= ';',
+  TOKEN_NONE			= 256,
+  TOKEN_STRING,
+};
+
+static struct scanner *scanner_new(FILE *input)
+{
+  struct scanner *s;
+
+  s = (struct scanner *)malloc(sizeof(struct scanner));
+  assert(s != NULL);
+
+  s->input = input;
+  s->cur_line = 1;
+  s->cur_position = 1;
+
+  s->token_text = g_string_new("");
+
+  return s;
+}
+
+static int scanner_cur_line(struct scanner *s)
+{
+  return s->cur_line;
+}
+
+static int scanner_cur_position(struct scanner *s)
+{
+  return s->cur_position;
+}
+
+static char *scanner_token_text(struct scanner *s)
+{
+  return s->token_text->str;
+}
+
+static int is_first_identifier(int c)
+{
+  return isalpha(c) || c == '_';
+}
+
+static int is_identifier(int c)
+{
+  return isalnum(c) || c == '-'  || c == '_';
+}
+
+int scanner_get_next_token(struct scanner *s)
+{
+  int c;
+  enum { 
+    S_INITIAL,
+    S_SPACE,
+    S_COMMENT,
+    S_IDENTIFIER,
+    S_INTEGER,
+    S_STRING,
+  } state;
+
+  state = S_INITIAL;
+  g_string_set_size(s->token_text, 0);
+
+  while(1) {
+    c = getc(s->input);
+
+    s->cur_position++;
+    if (c == '\n')
+      s->cur_line++;
+
+    if (c == EOF)
+      return TOKEN_EOF;
+
+    switch(state) {
+
+    case S_INITIAL:
+      if (isblank(c))
+	state = S_SPACE;
+      else if (c == '#')
+	state = S_COMMENT;
+      else if (c == '"')
+	state = S_STRING;
+      else if (is_first_identifier(c)) {
+	g_string_append_c(s->token_text, c);
+	state = S_IDENTIFIER;
+      } else if (isdigit(c)) {
+	g_string_append_c(s->token_text, c);
+	state = S_INTEGER;
+      } else if (c == '[' || c == ']' || c == '=' || c == ',' || c == ';') {  /* may be return char in any case ? */
+	g_string_append_c(s->token_text, c);
+	return c;
+      }
+      break;
+
+    case S_SPACE:
+      if (!isspace(c)) {
+	ungetc(c, s->input);
+	state = S_INITIAL;
+      }
+      break;
+
+    case S_COMMENT:
+      if (c == '\n')
+	state = S_INITIAL;
+      break;
+
+    case S_STRING:
+      if (c != '"')
+	g_string_append_c(s->token_text, c);
+      else
+	return TOKEN_STRING;
+      break;
+
+    case S_IDENTIFIER:
+      if (is_identifier(c))
+	g_string_append_c(s->token_text, c);
+      else {
+	ungetc(c, s->input);
+	return TOKEN_STRING;
+      }
+      break;
+
+    case S_INTEGER:
+      if (isdigit(c))
+	g_string_append_c(s->token_text, c);
+      else {
+	ungetc(c, s->input);
+	return TOKEN_STRING;
+      }
+      break;
+
+    }
+  }
+
+  return TOKEN_NONE;
+}
+
+static void scanner_free(struct scanner *s)
+{
+  g_string_free(s->token_text, TRUE);
+  free(s);
+}
+
 struct uhuru_conf_parser {
-  GScanner *scanner;
-  int fd;
-  guint lookahead_token;
+  struct scanner *scanner;
+  FILE *input;
+  enum token_type lookahead_token;
   jmp_buf env;
   struct uhuru_module *current_module;
   char *current_key;
@@ -56,30 +180,50 @@ struct uhuru_conf_parser *uhuru_conf_parser_new(const char *filename)
   cp = (struct uhuru_conf_parser *)malloc(sizeof(struct uhuru_conf_parser));
   assert(cp != NULL);
 
-  cp->scanner = g_scanner_new(&conf_parser_config);
-  
-  cp->fd = open(filename, O_RDONLY);
-  if (cp->fd < 0) {
+  cp->input = fopen(filename, "r");
+  if (cp->input == NULL) {
     g_log(NULL, G_LOG_LEVEL_WARNING, "cannot open conf file %s", filename);
   }
 
-  g_scanner_input_file(cp->scanner, cp->fd);
-
+  cp->scanner = scanner_new(cp->input);
+  
   return cp;
 }
 
-/* not defined in glib.h */
-#define G_TOKEN_SEMI_COLON  ';'
-
-static const char *token_str(guint token)
+static char *token2str(guint token)
 {
-  if (token == G_TOKEN_IDENTIFIER) 
-    return "identifier";
-  else if (token == G_TOKEN_STRING) 
+  switch(token) {
+#define M(E) case E: return #E
+    M(TOKEN_EOF);
+    M(TOKEN_LEFT_BRACE);
+    M(TOKEN_RIGHT_BRACE);
+    M(TOKEN_EQUAL_SIGN);
+    M(TOKEN_COMMA);
+    M(TOKEN_SEMI_COLON);
+    M(TOKEN_NONE);
+    M(TOKEN_STRING);
+  default:
+    return "???";
+  }
+
+  return "???";
+}
+
+static void print_token(struct scanner *scanner, enum token_type token)
+{
+  fprintf(stderr, "%-20s %3d ", token2str(token), token);
+  if (token == TOKEN_STRING) 
+    fprintf(stderr, " \"%s\"", scanner_token_text(scanner));
+  else
+    fprintf(stderr, " %c", (token < 255) ? (char)token : '?');
+  fprintf(stderr, "\n");
+}
+
+static const char *token_str(enum token_type token)
+{
+  if (token == TOKEN_STRING) 
     return "string";
-  else if (token == G_TOKEN_INT)
-    return "integer";
-  else if (token < G_TOKEN_NONE) {
+  else if (token < TOKEN_NONE) {
     /* memory leak, I know, I know, but this function is called only in case of syntax error... */
     char *tmp = (char *)malloc(2);
 
@@ -95,8 +239,8 @@ static const char *token_str(guint token)
 static void syntax_error(struct uhuru_conf_parser *cp, guint token) 
 {
   g_log(NULL, G_LOG_LEVEL_WARNING, "syntax error: at line %d position %d expecting '%s' got '%s'\n", 
-	g_scanner_cur_line(cp->scanner),
-	g_scanner_cur_position(cp->scanner),
+	scanner_cur_line(cp->scanner),
+	scanner_cur_position(cp->scanner),
 	token_str(token), 
 	token_str(cp->lookahead_token));
 
@@ -106,8 +250,12 @@ static void syntax_error(struct uhuru_conf_parser *cp, guint token)
 static void accept(struct uhuru_conf_parser *cp, guint token)
 {
   if (cp->lookahead_token == token) {
-    if (cp->lookahead_token != G_TOKEN_EOF)
-      cp->lookahead_token = g_scanner_get_next_token(cp->scanner);
+    if (cp->lookahead_token != TOKEN_EOF) {
+      cp->lookahead_token = scanner_get_next_token(cp->scanner);
+#if 1
+      print_token(cp->scanner, cp->lookahead_token);
+#endif
+    }
   } else
     syntax_error(cp, token);
 }
@@ -121,7 +269,7 @@ static void accept(struct uhuru_conf_parser *cp, guint token)
   definition_list: definition definition_list | EMPTY
   definition : STRING '=' value opt_value_list 
   opt_value_list : list_sep value opt_value_list | EMPTY
-  value : STRING | INT
+  value : STRING
   list_sep : ',' | ';' 
 */
 
@@ -143,7 +291,7 @@ static void r_configuration(struct uhuru_conf_parser *cp)
 /* group_list : group group_list | EMPTY */
 static void r_group_list(struct uhuru_conf_parser *cp)
 {
-  if (cp->lookahead_token == G_TOKEN_LEFT_BRACE) {
+  if (cp->lookahead_token == TOKEN_LEFT_BRACE) {
     r_group(cp);
     r_group_list(cp);
   }
@@ -152,16 +300,16 @@ static void r_group_list(struct uhuru_conf_parser *cp)
 /* group : '[' STRING ']' definition_list */
 static void r_group(struct uhuru_conf_parser *cp)
 {
-  accept(cp, G_TOKEN_LEFT_BRACE);
-  accept(cp, G_TOKEN_STRING);
-  accept(cp, G_TOKEN_RIGHT_BRACE);
+  accept(cp, TOKEN_LEFT_BRACE);
+  accept(cp, TOKEN_STRING);
+  accept(cp, TOKEN_RIGHT_BRACE);
   r_definition_list(cp);
 }
 
 /* definition_list: definition definition_list | EMPTY */
 static void r_definition_list(struct uhuru_conf_parser *cp)
 {
-  if (cp->lookahead_token == G_TOKEN_STRING) {
+  if (cp->lookahead_token == TOKEN_STRING) {
     r_definition(cp);
     r_definition_list(cp);
   }
@@ -170,8 +318,8 @@ static void r_definition_list(struct uhuru_conf_parser *cp)
 /* definition : STRING '=' value opt_value_list  */
 static void r_definition(struct uhuru_conf_parser *cp)
 {
-  accept(cp, G_TOKEN_STRING);
-  accept(cp, G_TOKEN_EQUAL_SIGN);
+  accept(cp, TOKEN_STRING);
+  accept(cp, TOKEN_EQUAL_SIGN);
   r_value(cp);
   r_opt_value_list(cp);
 }
@@ -179,29 +327,26 @@ static void r_definition(struct uhuru_conf_parser *cp)
 /* opt_value_list : list_sep value opt_value_list | EMPTY */
 static void r_opt_value_list(struct uhuru_conf_parser *cp)
 {
-  if (cp->lookahead_token == G_TOKEN_COMMA || cp->lookahead_token == G_TOKEN_SEMI_COLON) {
+  if (cp->lookahead_token == TOKEN_COMMA || cp->lookahead_token == TOKEN_SEMI_COLON) {
     r_list_sep(cp);
     r_value(cp);
     r_opt_value_list(cp);
   }
 }
 
-/* value : STRING | INT */
+/* value : STRING */
 static void r_value(struct uhuru_conf_parser *cp)
 {
-  if (cp->lookahead_token == G_TOKEN_STRING)
-    accept(cp, G_TOKEN_STRING);
-  else
-    accept(cp, G_TOKEN_INT);
+  accept(cp, TOKEN_STRING);
 }
 
 /* list_sep : ',' | ';'  */
 static void r_list_sep(struct uhuru_conf_parser *cp)
 {
-  if (cp->lookahead_token == G_TOKEN_COMMA)
-    accept(cp, G_TOKEN_COMMA);
+  if (cp->lookahead_token == TOKEN_COMMA)
+    accept(cp, TOKEN_COMMA);
   else
-    accept(cp, G_TOKEN_SEMI_COLON);
+    accept(cp, TOKEN_SEMI_COLON);
 }
 
 
@@ -209,12 +354,15 @@ int uhuru_conf_parser_parse(struct uhuru_conf_parser *cp)
 {
   int ret = 1;
 
-  cp->lookahead_token = g_scanner_get_next_token(cp->scanner);
+  cp->lookahead_token = scanner_get_next_token(cp->scanner);
+#if 1
+  print_token(cp->scanner, cp->lookahead_token);
+#endif
 
   if (!setjmp(cp->env)) {
     r_configuration(cp);
 
-    accept(cp, G_TOKEN_EOF);
+    accept(cp, TOKEN_EOF);
 
     ret = 0;
   }
@@ -224,9 +372,9 @@ int uhuru_conf_parser_parse(struct uhuru_conf_parser *cp)
 
 void uhuru_conf_parser_free(struct uhuru_conf_parser *cp)
 {
-  g_scanner_destroy(cp->scanner);
+  scanner_free(cp->scanner);
 
-  close(cp->fd);
+  fclose(cp->input);
 
   free(cp);
 }
