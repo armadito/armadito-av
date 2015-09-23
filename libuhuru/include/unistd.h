a52074d63deb174ba8f686f774e42b0d19c0b726
Ulrich <u.fausther@uhuru-solutions.com>
2015-09-23 12:10:20 +0200

    Uhuru-av-Windows :: ajout glib version 2.28 + headers magic.h / unistd.h / libuhuru-config.h

--- libuhuru/include/unistd.h
+++ libuhuru/include/unistd.h
@@ -1,4 +1,18 @@
 #ifndef _UNISTD_H
 #define _UNISTD_H
 
+#include <dirent.h>
+#include <sys/types.h>
+#include <direct.h>
+#define ssize_t int
+
+// to implement
+ssize_t read(int fd, void * buf, size_t count);
+int readdir_r(DIR * dirp, struct dirent * entry, struct dirent ** result);
+
+// in <sys/stat.h>
+// modif by ulrich: use mkdir function in direct.h
+#define mkdir(path, mode) _mkdir(path)
+
+
 #endif
---
+++ libuhuru/libuhuru-config.h
@@ -0,0 +1,8 @@
+#ifndef _LIBUHURU_CONF_H
+#define _LIBUHURU_CONF_H
+
+#define LIBUHURU_CONF_DIR "Path/to/conf/dir"
+#define LIBUHURU_MODULES_PATH "Path/to/module"
+
+
+#endif
