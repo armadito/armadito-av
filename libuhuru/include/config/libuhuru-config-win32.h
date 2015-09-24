c9240fb1aaba9d9b166453e5f5b47bde82dd40f4
François Déchelle <francois@dechelle.net>
2015-09-24 14:48:46 +0200

    * moved config include files to include/config; added stuff to have win32 config and autoconf generated config

---
+++ libuhuru/include/config/libuhuru-config-win32.h
@@ -0,0 +1,16 @@
+#ifndef _LIBUHURU_CONFIG_WIN32_H
+#define _LIBUHURU_CONFIG_WIN32_H
+
+/* features */
+#undef HAVE_UNIX_SOCKET
+
+/* headers */
+#undef HAVE_UNISTD_H 
+
+/* mapping posix names to win32 names */
+#define strdup _strdup
+
+/* file handling macros */
+#define MAXPATHLEN _MAX_PATH
+
+#endif
---
+++ libuhuru/include/config/libuhuru-config.h
@@ -0,0 +1,10 @@
+#ifndef _LIBUHURU_CONFIG_H_
+#define _LIBUHURU_CONFIG_H_
+
+#ifdef WIN32
+#include <libuhuru-config-win32.h>
+#else
+#include <libuhuru-config-autoconf.h>
+#endif
+
+#endif
