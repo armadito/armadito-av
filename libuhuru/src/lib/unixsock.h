5d1c927edfec018a5b56ea64fb59be8a6a932b97
Ulrich <u.fausther@uhuru-solutions.com>
2015-09-28 11:17:25 +0200

    Uhuru-av-Windows:: add #ifndef WIN32 on unixsocket.h

--- libuhuru/src/lib/unixsock.h
+++ libuhuru/src/lib/unixsock.h
@@ -1,6 +1,7 @@
 #ifndef _UNIXSOCK_H_
 #define _UNIXSOCK_H_
 
+#ifndef WIN32
 #include <unistd.h>
 
 int client_socket_create(const char *socket_path, int max_retry);
@@ -16,5 +17,6 @@ ssize_t read_n(int fd, char *buffer, size_t len);
 ssize_t write_c(int fd, char *buffer, size_t len);
 
 ssize_t write_n(int fd, char *buffer, size_t len);
+#endif
 
 #endif
