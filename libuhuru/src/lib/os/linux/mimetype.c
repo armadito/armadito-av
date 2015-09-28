952c8f7df629ae4dd5f57132c6a090559d53706a
François Déchelle <francois@dechelle.net>
2015-09-28 21:35:24 +0200

    api mime type implemented on linux

--- libuhuru/src/lib/os/linux/mimetype.c
+++ libuhuru/src/lib/os/linux/mimetype.c
@@ -8,34 +8,43 @@
 /* Unfortunately, libmagic is not thread-safe. */
 /* We create a new magic_t for each thread, and keep it  */
 /* in thread's private data, so that it is created only once. */
+
 static void magic_destroy_notify(gpointer data)
 {
   magic_close((magic_t)data);
 }
 
-static magic_t get_private_magic(struct uhuru_scan *scan)
+static magic_t get_private_magic(void)
 {
-  magic_t m = (magic_t)g_private_get(scan->local.private_magic_key);
+  static int init_done = 0;
+  static GPrivate *private_key;
+  magic_t m;
+
+  if (init_done) {
+    private_key = g_private_new(magic_destroy_notify);
+    init_done = 1;
+  }
+
+  m = (magic_t)g_private_get(private_key);
 
   if (m == NULL) {
     m = magic_open(MAGIC_MIME_TYPE);
     magic_load(m, NULL);
 
-    g_private_set(scan->local.private_magic_key, (gpointer)m);
+    g_private_set(private_key, (gpointer)m);
   }
 
   return m;
 }
 
-/* somewhere */
-/* scan->local.private_magic_key = g_private_new(magic_destroy_notify); */
-  /* GPrivate *private_magic_key; */
-
-const char *mime_type_guess(const char *path)
+const char *os_mime_type_guess(const char *path)
 {
+  magic_t m;
   const char *mime_type;
 
-  mime_type = magic_file(magic, path);
+  m = get_private_magic();
+
+  mime_type = magic_file(m, path);
 
   return mime_type;
 }
--- libuhuru/src/lib/os/mimetype.h
+++ libuhuru/src/lib/os/mimetype.h
@@ -2,13 +2,13 @@
 #define _LIBUHURU_OS_MIMETYPE_H_
 
 /**
- *      \fn const char *mime_type_guess(const char *path);
+ *      \fn const char *os_mime_type_guess(const char *path);
  *      \brief Returns the mime type of a file
  *
  *      \param[in] path the path of the file
  *
  *      \return the mime type as a string, NULL if not guessable
  */
-const char *mime_type_guess(const char *path);
+const char *os_mime_type_guess(const char *path);
 
 #endif
