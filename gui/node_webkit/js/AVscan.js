bd0fb91e476ab6f9096cb5bdd63b2a87286f8768
Valentin Hamon <vhamon@teclib.com>
2015-10-05 16:22:25 +0200

    Ajout séléction de dossier de scan, et déclenchement du scan

--- gui/node_webkit/js/AVscan.js
+++ gui/node_webkit/js/AVscan.js
@@ -1,7 +1,28 @@
 var scan;
+var selected_dir = "";
 
+// Function who asks libuhuru for a scan
 function new_ondemand_scan(){
+
+    if(selected_dir == ""){
+		console.log("Nothing selected.");
+		return;
+	}
 	
-	console.log("New On-demand scan !");
-	
+	console.log("New On-demand scan for directory :" + selected_dir);
+    // TODO: Communicate with libuhuru
 }
+
+// We add a listener onChange for directory selection
+function chooseFile(name) {
+	var chooser = document.querySelector(name);
+	chooser.addEventListener("change", function(evt) {
+	  selected_dir = this.value;
+	  console.log(this.value);
+	}, false);
+
+	chooser.click();  
+}
+
+// We add the listener when loading the page
+chooseFile('#fileDialog');
