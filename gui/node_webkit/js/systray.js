1a3bedfb206da4f44417b6029345ccd1354da464
Valentin Hamon <vhamon@teclib.com>
2015-10-05 14:27:10 +0200

    Création des trois menus du systray clicable et définition des fonctions onclick dans un fichier séparé

--- gui/node_webkit/js/systray.js
+++ gui/node_webkit/js/systray.js
@@ -13,21 +13,35 @@ function initSystray(){
 	 
 }
 
-function SystrayMenu()
-{
-	// Give it a menu
+// Create Menu and return it
+function SystrayMenu(){
+	
 	var menu = new gui.Menu();
 
-	var item = new gui.MenuItem({
-	  label: "Click me",
-	  click: function() {
-		console.log("I'm clicked");
-	  },
+	var TrayMenu1 = new gui.MenuItem({
+	  label: "On-demand scan",
+	  click: TrayMenu1_onclick,
 	  key: "s",
 	  modifiers: "ctrl-alt",
 	});
 	
-	menu.append(item);
+	var TrayMenu2 = new gui.MenuItem({
+      label: "AV Updates state",
+	  click: TrayMenu2_onclick,
+	  key: "s",
+	  modifiers: "ctrl-alt",
+	});
+	
+	var TrayMenu3 = new gui.MenuItem({
+      label: "About",
+	  click: TrayMenu3_onclick,
+	  key: "s",
+	  modifiers: "ctrl-alt",
+	});
+	
+	menu.append(TrayMenu1);
+	menu.append(TrayMenu2);
+	menu.append(TrayMenu3);
 	
 	return 	menu;
 }
--- gui/node_webkit/js/systray_onclick.js
+++ gui/node_webkit/js/systray_onclick.js
@@ -1,2 +1,21 @@
 
-// TODO
+// "On-demand scan"
+function TrayMenu1_onclick()
+{
+	// TODO
+	console.log("TrayMenu1 Clicked");
+}
+
+// "AV Updates state"
+function TrayMenu2_onclick()
+{
+	// TODO
+	console.log("TrayMenu2 Clicked");
+}
+
+// "About"
+function TrayMenu3_onclick()
+{
+	// TODO
+	console.log("TrayMenu3 Clicked");
+}
