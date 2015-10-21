c11d14db94c4e9c967b077f2bab1e3daea94f721
Ulrich <u.fausther@uhuru-solutions.com>
2015-10-21 16:03:22 +0200

    Uhuru-av-Windows :: Add UhuruSvc project + service skeleton : project containing sources for Scan service. Implemented : Service install / start / stop / uninstall . (start on demand).

---
+++ VS/UhuruAV/UhuruSvc/service.h
@@ -0,0 +1,25 @@
+#ifndef __SERVICE_H_
+#define __SERVICE_H_
+
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define SVCNAME TEXT("UhuruSvc")
+#define SVCDISPLAY TEXT("Uhuru Scan Service")
+
+
+int ServiceInstall( );
+int ServiceRemove( );
+VOID ReportSvcStatus(DWORD dwCurrentState, DWORD dwWin32ExitCode, DWORD dwWaitHint);
+void WINAPI ServiceCtrlHandler(DWORD dwCtrl);
+void LaunchServiceAction( );
+void ServiceInit( );
+void WINAPI ServiceMain(int argc, char ** argv);
+BOOLEAN ServiceLaunchAction( );
+void ServiceLaunch( );
+void ServiceStop( );
+
+
+#endif
