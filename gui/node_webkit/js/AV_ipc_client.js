d28d3cf841e7eba73cd2879f9997b2e342c47e81
Valentin Hamon <vhamon@teclib.com>
2015-10-21 16:00:34 +0200

    Petite réorganisation du code pour l'ipc en nw.js

--- gui/node_webkit/js/AV_ipc_client.js
+++ gui/node_webkit/js/AV_ipc_client.js
@@ -1,5 +1,4 @@
-var path = "mynamedpipe";
-var client_path = '\\\\.\\pipe\\'+ path;
+var client_path = "uhuruAV_ondemand";
 var client_id =7;
 var client_connnect_retrying = false;
 var client_isRetrying = false;
@@ -8,17 +7,71 @@ var client_retry = 1000;
 var data_encoding ='ascii';
 var client_socket;
 var client_connected = false;
+var net = require('net');
+var os = require('os');
 
-function write_to_AV(){
+// Named pipes on win32
+console.log("OS type : " + os.type());
+console.log("OS platform : " + os.platform());
+console.log("Arch : " + os.arch());
+console.log("OS Release number : "+ os.release());
+if(os.platform() == "win32")
+{
+	client_path = '\\\\.\\pipe\\'+ client_path;
+}
+
+
+function parse_json_buffer ( buffer ){
+	
+	// Note - JSON.parse can tie up the current thread because it is a synchronous method. So if you are planning to parse big JSON objects use a streaming json parser.		
+	var json_object = null;
+
+	try {
+		json_object = JSON.parse(buffer.toString('ascii'));
+	}
+	catch(e){
+		console.error("Parsing error:", e); 
+		return null;
+	}
+	
+	return json_object;
+}
+
+function write_to_AV( data ){
  	
-	var buff_to_write = new Buffer( '{"type":"Buffer","data":[116,101,115,116]}' , data_encoding );
+	var buff_to_write = new Buffer( data, data_encoding );
+	
+	// We check json_object before writing on socket to AV
+	if(!parse_json_buffer(buff_to_write))
+	{
+		console.log('Error on json_object what sould be sent to AV. Stopping action.');
+		return -1;
+	}
+	
 	client_socket.write(buff_to_write, data_encoding, function (){  console.log("data_written.");  } );
+	
+	return 0;
+}
+
+function read_from_AV ( data ) {
+	
+	var buff = new Buffer(data, data_encoding);
+	console.log(' received buffer ('+data_encoding+'): -' + buff +'-');
+	console.log(' received string -'+buff.toString()+'-');
+	
+	if(!parse_json_buffer(buff))
+	{
+		console.log('Error on json_object received from AV. Stopping action.');
+		return -1;
+	}
+
+	return 0;
 }
 
 function connect_to_AV(){
 
     console.log('Requested connection to ' + client_id+ ' ' + client_path);
-	console.log('Connecting client on Unix Socket :' + client_path);
+	console.log('Connecting client on Socket :' + client_path);
 	
 	client_socket = net.connect(
 		{
@@ -40,6 +93,7 @@ function connect_to_AV(){
         }
     );
 
+	// Retry mode TO BE TESTED
     client_socket.on(
         'close',
         function(){
@@ -68,18 +122,14 @@ function connect_to_AV(){
                             )
                         }
                     },
-                client_retry );
+             client_retry );
         }
     );
 
     client_socket.on(
         'data',
         function(data) {
-			
-            console.log('## recieved events ##');
-			var buff = new Buffer(data, data_encoding);
-			console.log(' received buffer ('+data_encoding+'): ' + buff);
-			return;
+			return read_from_AV(data);
         }
     );
 }
