214d476acb50a8bf7e9e89e197906954cdf13c55
François Déchelle <francois@dechelle.net>
2015-10-27 16:51:36 +0100

    added error print function

--- libuhuru/include/libuhuru/common/action.h
+++ libuhuru/include/libuhuru/common/action.h
@@ -7,14 +7,14 @@
 extern "C" {
 #endif
 
-const char *uhuru_action_pretty_str(enum uhuru_action action);
-
 enum uhuru_action {
   UHURU_ACTION_NONE         = 0,
   UHURU_ACTION_ALERT        = 1 << 1,
   UHURU_ACTION_QUARANTINE   = 1 << 2,
   UHURU_ACTION_REMOVE       = 1 << 3,
 };
+
+const char *uhuru_action_pretty_str(enum uhuru_action action);
 
 #ifdef __cplusplus
 }
