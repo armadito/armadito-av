1ada4cfdbea1347dffdfb041c86889015829e233
TounToun <v.hamon@uhuru-mobile.com>
2015-11-10 16:09:36 +0100

    Amélioration script packagin

--- scripts/package_all.sh
+++ scripts/package_all.sh
@@ -7,6 +7,7 @@ SRC_DIR=$DIR/../
 TOP_BUILDDIR=$OUT_DIR/build/$OS_V
 TOP_SOURCEDIR=$OUT_DIR/sources
 TOP_PKGBUILDDIR=$OUT_DIR/build/packages/$OS_V
+export GNUPGHOME=$HOME/uhuru-linux-packaging/gpg
 
 mkdir -p $TOP_SOURCEDIR
 mkdir -p $TOP_PKGBUILDDIR
@@ -19,5 +20,5 @@ cd $HOME/uhuru-linux-packaging
 ./autogen.sh
 ./scripts/mktarball.sh -r $OUT_DIR
 ./configure --with-tarballdir=$OUT_DIR/sources
-#make -C packages/ubuntu package
-#make -C packages/ubuntu upload
+make -C packages/ubuntu package
+make -C packages/ubuntu upload
