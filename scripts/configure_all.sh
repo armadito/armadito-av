453211d6ac191e8c0a17f5928d7edb0051affd9e
TounToun <v.hamon@uhuru-mobile.com>
2015-11-12 11:42:55 +0100

    Update scripts pour packaging UhuruAV

--- scripts/configure_all.sh
+++ scripts/configure_all.sh
@@ -9,11 +9,12 @@ mkdir -p $OUT_DIR/build/$OS_V/uhuru-av/libuhuru/pkgconfig
 mkdir -p $OUT_DIR/build/$OS_V/uhuru-av/gui
 mkdir -p $OUT_DIR/build/$OS_V/uhuru-av/modules/module5_2
 mkdir -p $OUT_DIR/build/$OS_V/uhuru-av/modules/clamav
+mkdir -p $OUT_DIR/build/$OS_V/uhuru-av/modules/fanotify
 
 set -e
 
-#cd $SRC_DIR
-#sudo chown -R uhuru:uhuru .
+cd $SRC_DIR
+sudo chown -R $USER:$USER .
 
 cd $SRC_DIR/libuhuru/
 sudo chmod +x autogen.sh
@@ -21,7 +22,7 @@ sudo chmod +x autogen.sh
 
 cd $OUT_DIR/build/$OS_V/uhuru-av/libuhuru
 echo "-------CONFIGURE-------"
-$SRC_DIR/libuhuru/configure --prefix=$OUT_DIR/install/$OS_V/uhuru-av
+$SRC_DIR/libuhuru/configure --prefix=$OUT_DIR/install/$OS_V/uhuru-av PKG_CONFIG_PATH=$OUT_DIR/install/$OS_V/uhuru-av/lib/pkgconfig
 echo "-------OKAY LIBUHURU-------"
 
 cd $SRC_DIR/modules/clamav/
@@ -30,8 +31,19 @@ sudo chmod +x autogen.sh
 
 cd $OUT_DIR/build/$OS_V/uhuru-av/modules/clamav
 echo "-------CONFIGURE-------"	
-$SRC_DIR/modules/clamav/configure --prefix=$OUT_DIR/install/$OS_V/uhuru-av
+$SRC_DIR/modules/clamav/configure --prefix=$OUT_DIR/install/$OS_V/uhuru-av PKG_CONFIG_PATH=$OUT_DIR/install/$OS_V/uhuru-av/lib/pkgconfig
 echo "-------OKAY CLAMAV-------"
+
+
+cd $SRC_DIR/modules/fanotify/
+sudo chmod +x autogen.sh
+./autogen.sh	
+
+cd $OUT_DIR/build/$OS_V/uhuru-av/modules/fanotify
+echo "-------CONFIGURE-------"
+$SRC_DIR/modules/fanotify/configure --prefix=$OUT_DIR/install/$OS_V/uhuru-av PKG_CONFIG_PATH=$OUT_DIR/install/$OS_V/uhuru-av/lib/pkgconfig
+echo "-------OKAY FANOTIFY-------"
+
 
 cd $SRC_DIR/modules/module5_2/
 sudo chmod +x autogen.sh
@@ -39,7 +51,7 @@ sudo chmod +x autogen.sh
 
 cd $OUT_DIR/build/$OS_V/uhuru-av/modules/module5_2
 echo "-------CONFIGURE-------"
-$SRC_DIR/modules/module5_2/configure --prefix=$OUT_DIR/install/$OS_V/uhuru-av
+$SRC_DIR/modules/module5_2/configure --prefix=$OUT_DIR/install/$OS_V/uhuru-av PKG_CONFIG_PATH=$OUT_DIR/install/$OS_V/uhuru-av/lib/pkgconfig
 echo "-------OKAY MODULE5_2-------"
 
 cd $SRC_DIR/gui/
@@ -48,7 +60,7 @@ sudo chmod +x autogen.sh
 
 cd $OUT_DIR/build/$OS_V/uhuru-av/gui
 echo "-------CONFIGURE-------"
-$SRC_DIR/gui/configure --prefix=$OUT_DIR/install/$OS_V/uhuru-av
+$SRC_DIR/gui/configure --prefix=$OUT_DIR/install/$OS_V/uhuru-av PKG_CONFIG_PATH=$OUT_DIR/install/$OS_V/uhuru-av/lib/pkgconfig
 echo "-------OKAY QTGUI-------"
 
 ## On lui indique où trouver les bases
--- scripts/package_all.sh
+++ scripts/package_all.sh
@@ -1,5 +1,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 OS_V=ubuntu-14.04-64
+PACKAGE_VERSION=1.5.1 
 
 OUT_DIR=$DIR/../out
 SRC_DIR=$DIR/../
@@ -16,9 +17,20 @@ set -e
 
 cd $HOME/uhuru-linux-packaging
 
+FULL_VERSION=$PACKAGE_VERSION-0ubuntu1+trusty1
+
 # Package all
 ./autogen.sh
 ./scripts/mktarball.sh -r $OUT_DIR
 ./configure --with-tarballdir=$OUT_DIR/sources
+make -C packages/ubuntu/libuhuru package
+
+# Install packages needed fcr dependances
+sudo dpkg -i --force-all packages/ubuntu/libuhuru/BUILD/libuhuru_"$FULL_VERSION"_amd64.deb 
+sudo dpkg -i --force-all packages/ubuntu/libuhuru/BUILD/libuhuru-dev_"$FULL_VERSION"_amd64.deb 
+sudo dpkg -i --force-all packages/ubuntu/libuhuru/BUILD/libuhuru-tools_"$FULL_VERSION"_amd64.deb
+
 make -C packages/ubuntu package
 make -C packages/ubuntu upload
+
+
