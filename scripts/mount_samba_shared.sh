b1ddecb3a259caedeb5f8488997c35134cbd68dd
TounToun <v.hamon@uhuru-mobile.com>
2015-11-19 09:22:07 +0100

    Ajout mount samba 12.04

--- scripts/mount_samba_shared.sh
+++ scripts/mount_samba_shared.sh
@@ -2,6 +2,10 @@
 
 # https://help.ubuntu.com/community/Samba/SambaClientGuide
 
-mkdir ~/mnt
-sudo mount -t cifs //n5samba/samba_shared ~/mnt -o username=uhuru,noexec
+mkdir -p ~/mnt/trusty
+mkdir ~/mnt/precise
+umount -l ~/mnt/trusty
+umount -l ~/mnt/precise
 
+sudo mount -t cifs //n5samba/samba_shared ~/mnt/trusty -o username=uhuru,noexec
+sudo mount -t cifs //n5precise32a/share ~/mnt/precise -o username=uhuru,noexec
