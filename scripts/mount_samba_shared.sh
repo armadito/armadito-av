16e495ca57613d0556958b2abe9db83b7eec9439
TounToun <v.hamon@uhuru-mobile.com>
2015-11-19 10:36:52 +0100

    Maj du script de montage des partitions samba + ajout dossier documentation

--- scripts/mount_samba_shared.sh
+++ scripts/mount_samba_shared.sh
@@ -2,10 +2,11 @@
 
 # https://help.ubuntu.com/community/Samba/SambaClientGuide
 
-mkdir -p ~/mnt/trusty
-mkdir ~/mnt/precise
-umount -l ~/mnt/trusty
-umount -l ~/mnt/precise
+mkdir -p ~/mnt/trusty_cifs
+mkdir -p ~/mnt/precise_cifs
 
-sudo mount -t cifs //n5samba/samba_shared ~/mnt/trusty -o username=uhuru,noexec
-sudo mount -t cifs //n5precise32a/share ~/mnt/precise -o username=uhuru,noexec
+sudo umount -l ~/mnt/trusty_cifs
+sudo umount -l ~/mnt/precise_cifs
+
+sudo mount -t cifs //n5samba/samba_shared ~/mnt/trusty_cifs -o username=uhuru,noexec
+sudo mount -t cifs //n5precise32a/share ~/mnt/precise_cifs -o username=uhuru,noexec
