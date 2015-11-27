80da44872d632714f8edfa377955b6afbf8b1fc2
François Déchelle <francois@dechelle.net>
2015-11-27 14:55:15 +0100

    adding log facility in libuhuru_core

--- libuhuru/src/libcore/log.c
+++ libuhuru/src/libcore/log.c
@@ -5,38 +5,77 @@
 #include <glib.h>
 #include <stdarg.h>
 
-#define G_LOG_DOMAIN "uhuru"
+static void stdouterr_handler(enum uhuru_log_domain domain, enum uhuru_log_level log_level, const char *message, void *user_data);
 
-static GLogLevelFlags g_log_level(enum uhuru_log_level level)
-{
-  switch(level) {
-  case UHURU_LOG_LEVEL_ERROR:
-    return G_LOG_LEVEL_ERROR;
-  case UHURU_LOG_LEVEL_WARNING:
-    return G_LOG_LEVEL_WARNING;
-  case UHURU_LOG_LEVEL_INFO:
-    return G_LOG_LEVEL_INFO;
-  case UHURU_LOG_LEVEL_DEBUG:
-    return G_LOG_LEVEL_DEBUG;
-  case UHURU_LOG_LEVEL_NONE:
-    return 1 << G_LOG_LEVEL_USER_SHIFT;
-  }
-
-  return -1;
-}
+static enum uhuru_log_level current_max_level = UHURU_LOG_LEVEL_ERROR;
+static uhuru_log_handler_t current_handler = stdouterr_handler;
+static void *current_handler_user_data = NULL;
 
 void uhuru_log(enum uhuru_log_domain domain, enum uhuru_log_level level, const char *format, ...)
 {
   va_list args;
+  GString *buff;
+  gchar *message;
 
+  /* anything to do? */
+  if (level > current_max_level)
+    return;
+
+  /* format message */
+  buff = g_string_new("");
   va_start(args, format);
-
-  g_logv(G_LOG_DOMAIN, g_log_level(level), format, args);
-
+  g_string_vprintf(buff, format, args);
   va_end(args);
+
+  /* call the handler with formated message */
+  message = g_string_free(buffer, FALSE);
+
+  (*current_handler)(level, message, current_handler_user_data);
+
+  g_free(message);
 }
 
 void uhuru_log_set_handler(enum uhuru_log_level max_level, uhuru_log_handler_t handler, void *user_data)
 {
+}
+
+static FILE *get_stream(enum uhuru_log_level log_level)
+{
+  return level & (UHURU_LOG_LEVEL_ERROR | UHURU_LOG_LEVEL_WARNING) ? stderr : stdout;
+}
+
+static const char *domain_str(enum uhuru_log_domain domain)
+{
+  switch(domain) {
+  case UHURU_LOG_LIBUHURU:
+    return "_lib";
+  case UHURU_LOG_MODULE:
+    return "_module";
+  case UHURU_LOG_SERVICE:
+    return "_service";
+  }
+
+  return "";
+}
+
+static void stdouterr_handler(enum uhuru_log_domain domain, enum uhuru_log_level log_level, const char *message, void *user_data)
+{
+  FILE *stream = get_stream(log_level);
+  GString *gstring = g_string_new(NULL);
+  gchar *string;
+
+  g_string_append_printf(gstring, "%s[%d]: ", domain_str(domain), os_getpid());
+
+  if (log_level < (1 << G_LOG_LEVEL_USER_SHIFT))
+    g_string_append_printf(gstring, "<%s> ", level_str(log_level));
+
+  g_string_append(gstring, message);
+  g_string_append(gstring, "\n");
+
+  string = g_string_free(gstring, FALSE);
+
+  fputs(string, stream);
+
+  g_free(string);
 }
 
