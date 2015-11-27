1a9a7551ecba4661f480e7ba8f14f5f7408d5ebb
Ulrich <u.fausther@uhuru-solutions.com>
2015-11-27 11:05:39 +0100

    uhuru-av-Windows :: add log in windows event log

--- VS/UhuruAV/ScanClient/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CLIENT_H__
-#define __CLIENT_H__
-
-#include "client.h"
-#include <Windows.h>
-
-int SvcReportEvent( WORD eventType  );
-
-#endif
--- libuhuru/src/libcore/action.c
+++ libuhuru/src/libcore/action.c
@@ -5,7 +5,7 @@
 const char *uhuru_action_pretty_str(enum uhuru_action action)
 {
 		//fprintf(stderr, "ClamAV initialization failed: %s\n", cl_strerror(ret));
-		g_log(NULL, 16, "DEBUG :: uhuru_action_pretty_str() :: %d ::\n",action);
+		//g_log(G_LOG_DOMAIN, G_LOG_LEVEL_DEBUG, "DEBUG :: uhuru_action_pretty_str() :: %d ::\n",action);
   switch(action & (UHURU_ACTION_ALERT | UHURU_ACTION_QUARANTINE | UHURU_ACTION_REMOVE)) {
   case UHURU_ACTION_ALERT: return "alert";
   case UHURU_ACTION_ALERT | UHURU_ACTION_QUARANTINE: return "alert+quarantine";
