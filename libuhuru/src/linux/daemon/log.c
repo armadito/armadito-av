4a88bb7560a3019ebbf26fc0e358568e3fdb8e90
François Déchelle <francois@dechelle.net>
2015-11-27 16:01:51 +0100

    adding log facility in libuhuru_core

--- libuhuru/src/linux/daemon/log.c
+++ libuhuru/src/linux/daemon/log.c
@@ -1,3 +1,5 @@
+#include <libuhuru/core.h>
+
 #include <glib.h>
 #include <syslog.h>
 #include <stdio.h>
@@ -107,7 +109,7 @@ static GLogLevelFlags get_log_level_from_str(const char *s_log_level)
   return -1;
 }
 
-void log_init(const char *s_log_level, int use_syslog)
+static void log_init_with_glog(const char *s_log_level, int use_syslog)
 {
   GLogLevelFlags flags, fatal_flags;
   
@@ -120,4 +122,52 @@ void log_init(const char *s_log_level, int use_syslog)
     g_log_set_handler(G_LOG_DOMAIN, flags, stderrout_handler, NULL);
 
   fatal_flags = g_log_set_fatal_mask(G_LOG_DOMAIN, G_LOG_LEVEL_ERROR);
+}
+
+/* **************************************************
+ *
+ * uhuru log version
+ *
+ * **************************************************
+ */
+
+static enum uhuru_log_level get_uhuru_log_level_from_str(const char *s_log_level)
+{
+  if (!strcmp(s_log_level,"error") || !strcmp(s_log_level,"critical"))
+    return UHURU_LOG_LEVEL_ERROR;
+
+  if (!strcmp(s_log_level,"warning"))
+    return UHURU_LOG_LEVEL_WARNING;
+
+  if (!strcmp(s_log_level,"message") || !strcmp(s_log_level,"info"))
+    return UHURU_LOG_LEVEL_INFO;
+
+  if (!strcmp(s_log_level,"debug"))
+    return UHURU_LOG_LEVEL_DEBUG;
+  return -1;
+}
+
+static void uhuru_syslog_handler(enum uhuru_log_domain domain, enum uhuru_log_level log_level, const char *message, void *user_data)
+{
+  if (log_level != UHURU_LOG_LEVEL_NONE)
+    syslog(priority_from_level(log_level), "<%s> %s\n", level_str(log_level), message);
+  else
+    syslog(priority_from_level(log_level), "%s\n", message);
+}
+
+static void log_init_with_uhuru_log(const char *s_log_level, int use_syslog)
+{
+  enum uhuru_log_level level = get_uhuru_log_level_from_str(s_log_level);
+
+  if (use_syslog) {
+    openlog("uhuru-av", LOG_CONS | LOG_PID, LOG_USER);
+    uhuru_log_set_handler(level, uhuru_syslog_handler, NULL);
+  } else
+    uhuru_log_set_handler(level, uhuru_log_default_handler, NULL);
+}
+
+void log_init(const char *s_log_level, int use_syslog)
+{
+  log_init_with_glog(s_log_level, use_syslog);
+  log_init_with_uhuru_log(s_log_level, use_syslog);
 }
