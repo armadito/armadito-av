dfc725f6a3d7bb0b74db85189918afa3c6ab36e4
François Déchelle <francois@dechelle.net>
2015-12-02 18:14:10 +0100

    adding scan configuration and file scan context for R.T.

--- libuhuru/include/libuhuru/libcore/scanconf.h
+++ libuhuru/include/libuhuru/libcore/scanconf.h
@@ -11,10 +11,10 @@ void uhuru_scan_conf_white_list_directory(struct uhuru_scan_conf *c, const char 
 
 int uhuru_scan_conf_is_white_listed(struct uhuru_scan_conf *c, const char *path);
 
-void uhuru_scan_conf_add_mime_type(struct uhuru_scan_conf *c, const char *mime_type, struct uhuru_module *module);
+void uhuru_scan_conf_add_mime_type(struct uhuru_scan_conf *c, const char *mime_type, const char *module_name, struct uhuru *u);
 
 struct uhuru_module **uhuru_scan_conf_get_applicable_modules(struct uhuru_scan_conf *c, const char *mime_type);
 
-void uhuru_scan_conf_file_size(struct uhuru_scan_conf *c, int max_file_size);
+void uhuru_scan_conf_max_file_size(struct uhuru_scan_conf *c, int max_file_size);
 
 #endif
--- modules/fanotify/src/fanotify.c
+++ modules/fanotify/src/fanotify.c
@@ -57,6 +57,49 @@ static enum uhuru_mod_status mod_fanotify_conf_set_enable_on_access(struct uhuru
   return UHURU_MOD_OK;
 }
 
+static enum uhuru_mod_status mod_fanotify_conf_white_list_dir(struct uhuru_module *module, const char *directive, const char **argv)
+{
+  struct fanotify_data *fa_data = (struct fanotify_data *)module->data;
+  struct uhuru_scan_conf *on_access_conf = uhuru_scan_conf_on_access();
+
+  while (*argv != NULL) {
+    uhuru_scan_conf_white_list_directory(on_access_conf, *argv);
+
+    argv++;
+  }
+
+  return UHURU_MOD_OK;
+}
+
+static enum uhuru_mod_status mod_fanotify_conf_mime_type(struct uhuru_module *module, const char *directive, const char **argv)
+{
+  struct fanotify_data *fa_data = (struct fanotify_data *)module->data;
+  const char *mime_type;
+  struct uhuru_scan_conf *on_access_conf = uhuru_scan_conf_on_access();
+
+  if (argv[0] == NULL || argv[1] == NULL) {
+    uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_WARNING, "fanotify: invalid configuration directive, not enough arguments");
+    return UHURU_MOD_CONF_ERROR;
+  }
+
+  mime_type = argv[0];
+
+  for(argv++; *argv != NULL; argv++)
+    uhuru_scan_conf_add_mime_type(on_access_conf, mime_type, *argv, module->uhuru);
+
+  return UHURU_MOD_OK;
+}
+
+static enum uhuru_mod_status mod_fanotify_conf_max_size(struct uhuru_module *module, const char *directive, const char **argv)
+{
+  struct fanotify_data *fa_data = (struct fanotify_data *)module->data;
+  struct uhuru_scan_conf *on_access_conf = uhuru_scan_conf_on_access();
+
+  uhuru_scan_conf_max_file_size(on_access_conf, atoi(argv[0]));
+  
+  return UHURU_MOD_OK;
+}
+
 static enum uhuru_mod_status mod_fanotify_post_init(struct uhuru_module *module)
 {
   struct fanotify_data *fa_data = (struct fanotify_data *)module->data;
@@ -86,6 +129,9 @@ static enum uhuru_mod_status mod_fanotify_close(struct uhuru_module *module)
 struct uhuru_conf_entry mod_fanotify_conf_table[] = {
   { "watch-dir", mod_fanotify_conf_set_watch_dir},
   { "enable-on-access", mod_fanotify_conf_set_enable_on_access},
+  { "white-list-dir", mod_fanotify_conf_white_list_dir},
+  { "mime-type", mod_fanotify_conf_mime_type},
+  { "max-size", mod_fanotify_conf_max_size},
   { NULL, NULL},
 };
 
