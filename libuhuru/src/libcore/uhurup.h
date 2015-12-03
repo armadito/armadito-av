840c2ad8aaacf7b437009f57ebf62191b2890041
François Déchelle <francois@dechelle.net>
2015-12-03 18:23:09 +0100

    updated libcore and fanotify

--- libuhuru/src/libcore/uhurup.h
+++ libuhuru/src/libcore/uhurup.h
@@ -9,11 +9,6 @@ struct uhuru_module **uhuru_get_modules(struct uhuru *u);
 
 struct uhuru_module *uhuru_get_module_by_name(struct uhuru *u, const char *name);
 
-void uhuru_add_mime_type(struct uhuru *u, const char *mime_type, struct uhuru_module *module);
-
-/* NULL terminated array of struct uhuru_module */
-struct uhuru_module **uhuru_get_applicable_modules(struct uhuru *u, const char *mime_type);
-
 #ifdef DEBUG
 const char *uhuru_debug(struct uhuru *u);
 #endif
