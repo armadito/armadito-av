0179eb71da552be69f621de05b98457d6c2e2bf6
François Déchelle <francois@dechelle.net>
2015-12-15 14:01:58 +0100

    update on-access test

--- libuhuru/tests/temps_reel/rtime/new/2/monitor.h
+++ libuhuru/tests/temps_reel/rtime/new/2/monitor.h
@@ -6,8 +6,9 @@ struct access_monitor;
 enum access_monitor_flags {
   MONITOR_MOUNT        = 1 << 0,
   MONITOR_RECURSIVE    = 1 << 1,
-  MONITOR_CHECK_TYPE   = 1 << 2,
+  MONITOR_TYPE_CHECK   = 1 << 2,
   MONITOR_LOG_EVENT    = 1 << 3,
+  MONITOR_ENABLE_PERM  = 1 << 4,
 };
 
 struct access_monitor *access_monitor_new(enum access_monitor_flags flags);
@@ -16,14 +17,16 @@ int access_monitor_get_poll_fd(struct access_monitor *m);
 
 void access_monitor_cb(void *user_data);
 
-int access_monitor_enable_permission(struct access_monitor *m, int enable_permission);
+int access_monitor_add(struct access_monitor *m, const char *path, unsigned int flags);
 
-int access_monitor_add(struct access_monitor *m, const char *path);
-
+#if 0
 int access_monitor_remove(struct access_monitor *m, const char *path);
+#endif
 
 void access_monitor_free(struct access_monitor *m);
 
+#if 0
 int access_monitor_activate(struct access_monitor *m);
+#endif
 
 #endif
