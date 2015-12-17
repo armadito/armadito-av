e4ffb4ae62ae294161a02291204e7d3c38dfd214
François Déchelle <francois@dechelle.net>
2015-12-17 18:09:53 +0100

    new implementation of on-access on linux

--- libuhuru/src/linux/modules/on-access-linux/src/monitor.c
+++ libuhuru/src/linux/modules/on-access-linux/src/monitor.c
@@ -33,16 +33,20 @@ struct access_monitor {
   pid_t my_pid;
 
   int start_pipe[2];
+
   int command_pipe[2];
+  GIOChannel *command_channel;
 
   int fanotify_fd;
   GIOChannel *notify_channel;
+
+  GThread *monitor_thread;
 };
 
 static gboolean access_monitor_start_cb(GIOChannel *source, GIOCondition condition, gpointer data);
 static gboolean access_monitor_command_cb(GIOChannel *source, GIOCondition condition, gpointer data);
 
-void access_monitor_thread_fun(gpointer data, gpointer user_data);
+static gpointer access_monitor_thread_fun(gpointer data);
 
 static void path_destroy_notify(gpointer data)
 {
@@ -93,7 +97,78 @@ struct access_monitor *access_monitor_new(struct uhuru *u)
   return m;
 }
 
+int access_monitor_start(struct access_monitor *m)
+{
+  char c = 'A';
+
+  if (m == NULL)
+    return 0;
+
+  if (write(m->start_pipe[1], &c, 1) < 0)
+    return -1;
+
+  return 0;
+}
+
 static gboolean access_monitor_start_cb(GIOChannel *source, GIOCondition condition, gpointer data)
 {
+  struct access_monitor *m = (struct access_monitor *)data;
+  char c;
+
+  if (read(m->start_pipe[0], &c, 1) < 0 || c != 'A') {
+    uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_ERROR, "fanotify: read() in activation callback failed (%s)", strerror(errno));
+
+    return FALSE;
+  }
+
+  uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_DEBUG, "fanotify: started");
+
+  g_io_channel_shutdown(source, FALSE, NULL);
+
+  m->monitor_thread = g_thread_new("access monitor thread", access_monitor_thread_fun, m);
+
   return TRUE;
 }
+
+static gpointer access_monitor_thread_fun(gpointer data)
+{
+  struct access_monitor *m = (struct access_monitor *)data;
+  GMainLoop *loop;
+
+  uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_DEBUG, "fanotify: started thread");
+
+  m->command_channel = g_io_channel_unix_new(m->command_pipe[0]);	
+  g_io_add_watch(m->command_channel, G_IO_IN, access_monitor_command_cb, m);
+
+  /* add the fanotify file desc to the thread loop */
+
+  /* if configured, add the mount monitor */
+
+  /* init all fanotify mark */
+
+  loop = g_main_loop_new(NULL, FALSE);
+
+  g_main_loop_run(loop);
+}
+
+static gboolean access_monitor_command_cb(GIOChannel *source, GIOCondition condition, gpointer data)
+{
+  struct access_monitor *m = (struct access_monitor *)data;
+  char cmd;
+
+  if (read(m->command_pipe[0], &cmd, 1) < 0) {
+    uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_ERROR, "fanotify: read() in command callback failed (%s)", strerror(errno));
+
+    return FALSE;
+  }
+
+  switch(cmd) {
+  case 'g':
+    break;
+  case 's':
+    break;
+  }
+
+  return TRUE;
+}
+
