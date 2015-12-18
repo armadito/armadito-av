bb43a74a94fa5fcaea401660b8ff211042b368d1
François Déchelle <francois@dechelle.net>
2015-12-18 11:20:17 +0100

    new implementation of on-access on linux; compile but not tested

--- libuhuru/src/linux/modules/on-access-linux/src/monitor.h
+++ libuhuru/src/linux/modules/on-access-linux/src/monitor.h
@@ -10,9 +10,10 @@ int access_monitor_enable(struct access_monitor *m, int enable);
 int access_monitor_enable_permission(struct access_monitor *m, int enable_permission);
 
 int access_monitor_enable_removable_media(struct access_monitor *m, int enable_removable_media);
-int access_monitor_add_mount(struct access_monitor *m, const char *mount_point);
 
-int access_monitor_add_path(struct access_monitor *m, const char *path);
+void access_monitor_add_mount(struct access_monitor *m, const char *mount_point);
+
+void access_monitor_add_path(struct access_monitor *m, const char *path);
 
 int access_monitor_start(struct access_monitor *m);
 
