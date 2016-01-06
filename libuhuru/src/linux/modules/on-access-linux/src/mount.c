e42fb159cd87c1266a4527e6ef278a7bf49a627c
François Déchelle <francois@dechelle.net>
2016-01-06 16:41:27 +0100

    reimplemented D-Bus stuff for mount notification, using system bus

--- libuhuru/src/linux/modules/on-access-linux/src/mount.c
+++ libuhuru/src/linux/modules/on-access-linux/src/mount.c
@@ -9,17 +9,81 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef SESSION_BUS
+#define MON_DBUS_TYPE          G_BUS_TYPE_SESSION
 #define MOUNT_INTERFACE        "org.gtk.Private.RemoteVolumeMonitor"
 #define MOUNT_ADD_MEMBER       "MountAdded"
 #define MOUNT_REMOVE_MEMBER    "MountRemoved"
+#else
+#define MON_DBUS_TYPE          G_BUS_TYPE_SYSTEM
+#define MON_DBUS_INTERFACE     "org.freedesktop.DBus.Properties"
+#define MON_DBUS_MEMBER        "PropertiesChanged"
+#endif
 
 struct mount_monitor {
+  GDBusConnection *conn;
+  guint signal_sub_id;
   mount_cb_t cb;
   void *user_data;
-  GDBusConnection *conn;
-  guint sub_add_id;
-  guint sub_remove_id;
 };
+
+static void debug_variant(GVariant *parameters, const char *msg)
+{
+  int argc;
+
+  fprintf(stderr, "%s: parameters %ld children type_string %s\n", msg, g_variant_n_children(parameters), g_variant_get_type_string(parameters));
+
+  for(argc = 0; argc < g_variant_n_children(parameters); argc++) {
+    GVariant *argv = g_variant_get_child_value(parameters, argc);
+
+    fprintf(stderr, "parameter %d -> type %s\n", argc, g_variant_get_type_string(argv));
+
+    g_variant_unref(argv);
+  }
+}
+
+static int get_mount_event(GVariant *parameters, enum mount_event_type *pev_type, const char **pmount_point)
+{
+  GVariant *arg0, *arg1, *arg2, *val;
+  const char *prop_name;
+  const char **mount_points;
+  gsize n_mount_points;
+
+  if (g_variant_n_children(parameters) < 3)
+    return 0;
+    
+  arg0 = g_variant_get_child_value(parameters, 0);
+  arg1 = g_variant_get_child_value(parameters, 1);
+  arg2 = g_variant_get_child_value(parameters, 2);
+
+  if (!g_variant_is_of_type(arg0, G_VARIANT_TYPE_STRING)
+      || !g_variant_is_of_type(arg1, G_VARIANT_TYPE_VARDICT)
+      || !g_variant_is_of_type(arg2, G_VARIANT_TYPE_STRING_ARRAY))
+    return 0;
+
+  prop_name = g_variant_get_string(arg0, NULL);
+  if (strcmp(prop_name, "org.freedesktop.UDisks2.Filesystem"))
+    return 0;
+
+  val = g_variant_lookup_value(arg1, "MountPoints", NULL);
+  if (val == NULL)
+    return 0;
+
+  debug_variant(val, "key value");
+
+  mount_points = g_variant_get_bytestring_array(val, &n_mount_points);
+
+  if (n_mount_points > 0) {
+    *pmount_point = mount_points[0];
+    *pev_type = EVENT_MOUNT;
+  } else {
+    *pev_type = EVENT_UMOUNT;
+  }    
+
+  g_free(mount_points);
+
+  return 1;
+}
 
 static void mount_cb(GDBusConnection *conn,
 		     const gchar *sender_name,
@@ -29,14 +93,12 @@ static void mount_cb(GDBusConnection *conn,
 		     GVariant *parameters,
 		     gpointer user_data)
 {
-  int argc;
-  GVariant *argv, *sub_argv;
-  const gchar *mount_dir;
   struct mount_monitor *m = (struct mount_monitor *)user_data;
-  enum mount_event_type ev_type = !strcmp(signal_name, "MountAdded") ? EVENT_MOUNT : EVENT_UMOUNT;
+  enum mount_event_type ev_type = EVENT_UNKNOWN;
+  const char *mount_point = NULL;
 
 #ifdef DEBUG
-  printf("mount_cb sender_name %s object_path %s interface_name %s signal_name %s\n", sender_name, object_path, interface_name, signal_name);
+  fprintf(stderr, "mount_cb: sender_name %s object_path %s interface_name %s signal_name %s\n", sender_name, object_path, interface_name, signal_name);
 #endif
 
   if (parameters == NULL) {
@@ -44,57 +106,27 @@ static void mount_cb(GDBusConnection *conn,
     return;
   }
 
-  if (g_variant_n_children(parameters) < 3) {
-    fprintf(stderr, "%ld parameters???\n", g_variant_n_children(parameters));
-    return;
-  }
-  
 #ifdef DEBUG
-  printf("parameters type %s\n", g_variant_get_type_string(parameters));
-  printf("got %ld parameters\n", g_variant_n_children(parameters));
-
-  for(argc = 0; argc < g_variant_n_children(parameters); argc++) {
-    argv = g_variant_get_child_value(parameters, argc);
-    printf("parameter %d -> type %s\n", argc, g_variant_get_type_string(argv));
-    g_variant_unref(argv);
-  }
+  debug_variant(parameters, "parameters");
 #endif
 
-  argv = g_variant_get_child_value(parameters, 2);
-  if (g_variant_n_children(argv) < 6) {
-    fprintf(stderr, "%ld sub-parameters???\n", g_variant_n_children(argv));
+  if (!get_mount_event(parameters, &ev_type, &mount_point))
     return;
-  }
 
-  sub_argv = g_variant_get_child_value(argv, 5);
-
-  if (!g_variant_is_of_type(sub_argv, G_VARIANT_TYPE_STRING)) {
-    fprintf(stderr, "sub-parameter 5 of type %s???\n", g_variant_get_type_string(sub_argv));
-    return;
-  }
-
-  mount_dir = g_variant_get_string(sub_argv, NULL);
-
-  if (!strncmp(mount_dir, "file://", 7))
-    (*m->cb)(ev_type, mount_dir + 7, m->user_data);
-
-  g_variant_unref(sub_argv);
-  g_variant_unref(argv);
+  (*m->cb)(ev_type, mount_point, m->user_data);
 }
 
 static void mount_monitor_subscribe_signals(struct mount_monitor *m)
 {
-  m->sub_add_id = g_dbus_connection_signal_subscribe(m->conn, NULL, MOUNT_INTERFACE, MOUNT_ADD_MEMBER, NULL, NULL, 0, mount_cb, m, NULL);
-  m->sub_remove_id = g_dbus_connection_signal_subscribe(m->conn, NULL, MOUNT_INTERFACE, MOUNT_REMOVE_MEMBER, NULL, NULL, 0, mount_cb, m, NULL);
+  m->signal_sub_id = g_dbus_connection_signal_subscribe(m->conn, NULL, MON_DBUS_INTERFACE, MON_DBUS_MEMBER, NULL, NULL, 0, mount_cb, m, NULL);
 
-  fprintf(stderr, "D-Bus connections %d %d", m->sub_add_id, m->sub_remove_id);
+  fprintf(stderr, "subscribed to D-Bus signal interface %s id=%d\n", MON_DBUS_INTERFACE, m->signal_sub_id);
   /* uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_INFO, "D-Bus connections %d %d", m->sub_add_id, m->sub_remove_id); */
 }
 
 static void mount_monitor_unsubscribe_signals(struct mount_monitor *m)
 {
-  g_dbus_connection_signal_unsubscribe(m->conn, m->sub_add_id);
-  g_dbus_connection_signal_unsubscribe(m->conn, m->sub_remove_id);
+  g_dbus_connection_signal_unsubscribe(m->conn, m->signal_sub_id);
 }
 
 struct mount_monitor *mount_monitor_new(mount_cb_t cb, void *user_data)
@@ -102,7 +134,8 @@ struct mount_monitor *mount_monitor_new(mount_cb_t cb, void *user_data)
   struct mount_monitor *m = malloc(sizeof(struct mount_monitor));
   GError *error = NULL;
 
-  m->conn = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, &error);
+  m->conn = g_bus_get_sync(MON_DBUS_TYPE, NULL, &error);
+
   if (m->conn == NULL) {
     /* uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_WARNING, "error getting connection to D-Bus (%s)", error->message); */
     fprintf(stderr, "error getting connection to D-Bus (%s)", error->message);
