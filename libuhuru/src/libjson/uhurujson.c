04578812b725eb9d6004a8b26d5bb51a058178dd
François Déchelle <francois@dechelle.net>
2016-01-14 12:07:01 +0100

    json request processing in progress

--- libuhuru/src/libjson/uhurujson.c
+++ libuhuru/src/libjson/uhurujson.c
@@ -1,9 +1,58 @@
 #include "libuhuru-config.h"
 
+#include <libuhuru/core.h>
+
 #include "uhurujson.h"
 
-void foo(struct json_object *obj)
+#include <assert.h>
+#include <json.h>
+#include <stdlib.h>
+
+/* check if a struct json_tokener * can be used */
+/* if called from multiple threads, no */
+
+struct uhuru_json_handler {
+  struct json_tokener *tokener;
+};
+
+struct uhuru_json_handler *uhuru_json_handler_new(void)
 {
-  json_object_to_file("/var/tmp/json-debug", obj);
+  struct uhuru_json_handler *jh = malloc(sizeof(struct uhuru_json_handler));
+
+  jh->tokener = json_tokener_new();
+  assert(jh->tokener != NULL);
+
+  return jh;
 }
 
+void uhuru_json_handler_free(struct uhuru_json_handler *jh)
+{
+  json_tokener_free(jh->tokener);
+  free(jh);
+}
+
+static int validate_request(struct json_object *json_req)
+{
+  return 0;
+}
+
+enum uhuru_json_status uhuru_json_handler_process_request(struct uhuru_json_handler *jh, const char *req, int req_len, struct uhuru *uhuru, char **p_resp, int *p_resp_len)
+{
+  struct json_object *json_req;
+
+  json_tokener_reset(jh->tokener);
+
+  json_req = json_tokener_parse_ex(jh->tokener, req, req_len);
+
+  if (json_req == NULL) {
+    enum json_tokener_error jerr = json_tokener_get_error(jh->tokener);
+
+    uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_WARNING, "error in JSON parsing: %s", json_tokener_error_desc(jerr));
+    return JSON_PARSE_ERROR;
+  }
+
+  if (!validate_request(json_req))
+    return JSON_INVALID_REQUEST;
+
+  return 0;
+}
--- libuhuru/src/libjson/uhurujson.h
+++ libuhuru/src/libjson/uhurujson.h
@@ -1,9 +1,20 @@
 #ifndef _UHURUJSON_H_
 #define _UHURUJSON_H_
 
-#include <json.h>
+enum uhuru_json_status {
+  JSON_OK = 0,
+  JSON_PARSE_ERROR = 100,
+  JSON_INVALID_REQUEST = 101,
+  JSON_REQUEST_FAILED = 200,
+};
 
-void foo(struct json_object *obj);
+struct uhuru_json_handler;
+
+struct uhuru_json_handler *uhuru_json_handler_new(void);
+
+void uhuru_json_handler_free(struct uhuru_json_handler *jh);
+
+enum uhuru_json_status uhuru_json_handler_process_request(struct uhuru_json_handler *jh, const char *req, int req_len, struct uhuru *uhuru, char **p_resp, int *p_resp_len);
 
 #endif
 
