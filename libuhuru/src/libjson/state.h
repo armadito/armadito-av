20ae0db33b6e850ffd33efcc172efbbf273fbbae
François Déchelle <francois@dechelle.net>
2016-01-15 17:40:40 +0100

    first version of JSON protocol working on linux; "state" request and response validated

--- libuhuru/src/libjson/state.h
+++ libuhuru/src/libjson/state.h
@@ -1,6 +1,6 @@
 #ifndef _STATE_H_
 #define _STATE_H_
 
-enum uhuru_json_status state_request(const char *request, int id, struct json_object *params, struct uhuru *uhuru, struct json_object **p_info, const char **error_message);
+enum uhuru_json_status state_request_cb(const char *request, int id, struct json_object *params, struct uhuru *uhuru, struct json_object **p_info, const char **error_message);
 
 #endif
