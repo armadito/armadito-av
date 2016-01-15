55b2af7fbba0ba9e7ca73906bcdc9b69f2c84818
François Déchelle <francois@dechelle.net>
2016-01-15 12:07:43 +0100

    added json object printer; adding AV state request handling

---
+++ libuhuru/src/libjson/print.c
@@ -0,0 +1,100 @@
+#include "print.h"
+
+static void json_print_obj(struct json_object *obj, FILE *out, int level);
+static void json_print_array(struct json_object *obj, FILE *out, int level);
+static void json_print_value(struct json_object *obj, FILE *out, int level);
+
+static void json_print_value(struct json_object *obj, FILE *out, int level)
+{
+  int i;
+
+  switch(json_object_get_type(obj)) {
+  case json_type_null:
+    fprintf(out, "(null)");
+    break;
+  case json_type_boolean:
+    fprintf(out, "%s", json_object_get_boolean(obj) ? "true" : "false");
+    break;
+  case json_type_double:
+    fprintf(out, "%f", json_object_get_double(obj));
+    break;
+  case json_type_int:
+    fprintf(out, "%d", json_object_get_int(obj));
+    break;
+  case json_type_string:
+    fprintf(out, "\"%s\"", json_object_get_string(obj));
+    break;
+  case json_type_object:
+    json_print_obj(obj, out, level);
+    break;
+  case json_type_array:
+    json_print_array(obj, out, level);
+  }
+}
+
+static void space(FILE *out, int level)
+{
+  int i;
+
+  for(i = 0; i < level; i++)
+    fprintf(out, " ");
+}
+
+static void json_print_obj(struct json_object *obj, FILE *out, int level)
+{
+  int first = 1;
+
+  fprintf(out, "{\n");
+
+  json_object_object_foreach(obj, key, val) {
+    if (!first) {
+      fprintf(out, ",\n");
+    } else
+      first = 0;
+
+    space(out, level + 2);
+
+    fprintf(out, "\"%s\": ", key);
+    json_print_value(val, out, level + 2);
+  }
+
+  fprintf(out, "\n");
+
+  space(out, level);
+
+  fprintf(out, "}");
+}
+
+static void json_print_array(struct json_object *obj, FILE *out, int level)
+{
+  int i;
+  struct json_object *val;
+  int first = 1;
+
+  fprintf(out, "[\n");
+
+  for(i = 0; i < json_object_array_length(obj); i++) {
+    int i;
+
+    if (!first) {
+      fprintf(out, ",\n");
+    } else
+      first = 0;
+
+    space(out, level + 2);
+
+    val = json_object_array_get_idx(obj, i);
+    json_print_value(val, out, level + 2);
+  }
+
+  fprintf(out, "\n");
+
+  space(out, level);
+
+  fprintf(out, "]");
+}
+
+void uhuru_json_print(struct json_object *obj, FILE *out)
+{
+  json_print_value(obj, out, 0);
+}
---
+++ libuhuru/src/libjson/state.c
@@ -0,0 +1,104 @@
+#include "libuhuru-config.h"
+
+#include <libuhuru/core.h>
+
+#include "uhurujson.h"
+#include "state.h"
+
+#include <json.h>
+#include <stdlib.h>
+#include <string.h>
+
+static struct json_object *antivirus_json(struct uhuru_info *info)
+{
+  struct json_object *j_antivirus;
+
+  j_antivirus = json_object_new_object();
+
+  json_object_object_add(j_antivirus, "version", json_object_new_string("3.14"));
+  json_object_object_add(j_antivirus, "service", json_object_new_string("on"));
+  json_object_object_add(j_antivirus, "real-time-protection", json_object_new_string("on"));
+  
+  return j_antivirus;
+}
+
+static struct json_object *update_status_json(enum uhuru_update_status status)
+{
+  switch(status) {
+  case UHURU_UPDATE_OK:
+    return json_object_new_string("up-to-date");
+  case UHURU_UPDATE_LATE:
+    return json_object_new_string("late");
+  case UHURU_UPDATE_CRITICAL:
+    return json_object_new_string("critical");
+  case UHURU_UPDATE_NON_AVAILABLE:
+    return json_object_new_string("non-available");
+  }
+
+  return json_object_new_string("unknown");
+}
+
+static struct json_object *update_json(enum uhuru_update_status status, const char *update_date)
+{
+  struct json_object *j_update;
+
+  j_update = json_object_new_object();
+  
+  json_object_object_add(j_update, "status", update_status_json(status));
+  json_object_object_add(j_update, "last-update", json_object_new_string(update_date));
+
+  return j_update;
+}
+
+static struct json_object *state_json(struct uhuru_info *info)
+{
+  struct json_object *j_state, *j_mod_array;
+  struct uhuru_module_info **m;
+
+  j_state = json_object_new_object();
+
+  json_object_object_add(j_state, "antivirus", antivirus_json(info));
+
+  json_object_object_add(j_state, "update", update_json(info->global_status, "1970-01-01 00:00"));
+
+  j_mod_array = json_object_new_array();
+
+  for(m = info->module_infos; *m != NULL; m++) {
+    struct json_object *j_mod = json_object_new_object();
+
+    json_object_object_add(j_mod, "name", json_object_new_string((*m)->name));
+    json_object_object_add(j_mod, "version", json_object_new_string("0.0.0"));
+    json_object_object_add(j_mod, "update", update_json((*m)->mod_status, (*m)->update_date));
+
+    json_object_array_add(j_mod_array, j_mod);
+  }
+
+  json_object_object_add(j_state, "modules", j_mod_array);
+
+  return j_state;
+}
+
+enum uhuru_json_status state_request(const char *request, int id, struct json_object *params, struct uhuru *uhuru, struct json_object **p_info, const char **p_error_message)
+{
+  struct uhuru_info *info;
+
+#ifdef DEBUG
+  uhuru_log(UHURU_LOG_SERVICE, UHURU_LOG_LEVEL_DEBUG, "ipc: info handler called");
+#endif
+
+  info = uhuru_info_new(uhuru);
+
+  if (info == NULL) {
+    *p_info = NULL;
+    *p_error_message = strdup("getting info failed...");
+
+    return JSON_REQUEST_FAILED;
+  }
+
+  *p_info = state_json(info);
+
+  uhuru_info_free(info);
+
+  return JSON_OK;
+}
+
