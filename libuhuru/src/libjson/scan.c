eded008f81a8337291e16ba878c56c22d6ff3b89
François Déchelle <francois@dechelle.net>
2016-02-01 11:00:55 +0100

    reorg network code (unix + tcp sockets)

--- libuhuru/src/libjson/scan.c
+++ libuhuru/src/libjson/scan.c
@@ -4,10 +4,16 @@
 
 #include "uhurujson.h"
 #include "scan.h"
+#ifdef linux
+#include "net/unixsockclient.h"
+#endif
 
 #include <json.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define DEFAULT_SOCKET_PATH   "/tmp/.uhuru-ihm"
 
 /*
   JSON object fields examples:
@@ -22,6 +28,8 @@
 static struct json_object *report_json(struct uhuru_report *report)
 {
   struct json_object *j_report;
+
+  j_report = json_object_new_object();
 
   json_object_object_add(j_report, "progress", json_object_new_int(report->progress));
 
@@ -40,33 +48,57 @@ static struct json_object *report_json(struct uhuru_report *report)
   return j_report;
 }
 
+/*
+{ "ihm_request":"scan", 
+  "id":123, 
+  "params": {
+    "progress": "70",
+    "path": "C://cygwin64//home//malware.exe", 
+    "scan_status": "malware", 
+    "scan_action": "alert+quarantine", 
+    "mod_name": "clamav", 
+    "mod_report": "Trojan.Mal32-77"
+  }
+}
+*/
+
 static void scan_callback(struct uhuru_report *report, void *callback_data)
 {
-#if 0
-  char * response;
-  const char * message;
-  HANDLE * hPipe = (HANDLE*)malloc(sizeof(HANDLE));
+  int fd;
+  struct json_object *j_request;
+  const char *req;
+  size_t req_len;
 
-  if (connect_to_IHM(report->scan_id, hPipe) < 0){
-    printf("Error when trying to connect to \\\\.\\pipe\\IHM_scan_%d \n", report->scan_id);
+  uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_DEBUG, "scan_callback");
+
+  fd = unix_client_connect(DEFAULT_SOCKET_PATH, 10);
+
+  if (fd < 0) {
+    uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_WARNING, "error connecting to IHM (%s)", strerror(errno));
     return;
   }
 
-  message = json_get_report_msg(report);
+  uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_DEBUG, "scan_callback: fd = %d", fd);
 
-  if (send_message_to_IHM(hPipe, (char*)message, &response) < 0){
-    printf("Error when writing callback msg on Pipe");
-    return;
-  }
+  j_request = json_object_new_object();
+  json_object_object_add(j_request, "ihm_request", json_object_new_string("scan"));
+  json_object_object_add(j_request, "id", json_object_new_int(report->scan_id));
+  json_object_object_add(j_request, "params", report_json(report));
 
-  // Traiter la réponse ici
-  closeConnection_to_IHM(hPipe);
-#endif
+  req = json_object_to_json_string(j_request);
+
+  uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_DEBUG, "scan_callback: req = %s", req);
+
+  if (write(fd, req, strlen(req)) < 0)
+    uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_WARNING, "error writing JSON response (%s)", strerror(errno));
+
+  close(fd);
 }
 
 enum uhuru_json_status scan_request_cb(const char *request, int id, struct json_object *params, struct uhuru *uhuru, struct json_object **p_info, const char **p_error_message)
 {
   struct json_object *j_path;
+  const char *path;
   struct uhuru_on_demand *on_demand;
 
 #ifdef DEBUG
@@ -84,9 +116,13 @@ enum uhuru_json_status scan_request_cb(const char *request, int id, struct json_
 
   *p_info = NULL;
 
-  on_demand = uhuru_on_demand_new(uhuru, id, json_object_get_string(j_path), UHURU_SCAN_RECURSE);
+  path = json_object_get_string(j_path);
 
-  uhuru_scan_add_callback(uhuru_on_demand_get_scan(on_demand), scan_callback, p_info);
+  uhuru_log(UHURU_LOG_SERVICE, UHURU_LOG_LEVEL_DEBUG, "JSON: scan path = %s", path);
+
+  on_demand = uhuru_on_demand_new(uhuru, id, path, UHURU_SCAN_RECURSE);
+
+  uhuru_scan_add_callback(uhuru_on_demand_get_scan(on_demand), scan_callback, NULL);
 
   uhuru_on_demand_run(on_demand);
 
