4d008ece8cd515018173ad9a67be6c1a97b76c01
François Déchelle <francois@dechelle.net>
2016-02-09 18:16:52 +0100

    fixed crash of daemon when receiving invalid requests from IHM; improved logging

--- libuhuru/src/libjson/print.c
+++ libuhuru/src/libjson/print.c
@@ -1,97 +1,107 @@
 #include "print.h"
 
-static void json_print_object(struct json_object *obj, FILE *out, int level);
-static void json_print_array(struct json_object *obj, FILE *out, int level);
-static void json_print_value(struct json_object *obj, FILE *out, int level);
+#include <glib.h>
 
-static void space(FILE *out, int level)
+static void json_object_str(struct json_object *obj, GString *out, int level);
+static void json_array_str(struct json_object *obj, GString *out, int level);
+static void json_value_str(struct json_object *obj, GString *out, int level);
+
+static void space(GString *out, int level)
 {
   while (level--)
-    fprintf(out, " ");
+    g_string_append_c(out, ' ');
 }
 
-static void json_print_object(struct json_object *obj, FILE *out, int level)
+static void json_object_str(struct json_object *obj, GString *out, int level)
 {
   int first = 1;
 
-  fprintf(out, "{\n");
+  g_string_append(out, "{\n");
 
   json_object_object_foreach(obj, key, val) {
     if (!first) {
-      fprintf(out, ",\n");
+      g_string_append(out, ",\n");
     } else
       first = 0;
 
     space(out, level + 2);
 
-    fprintf(out, "\"%s\": ", key);
-    json_print_value(val, out, level + 2);
+    g_string_append_printf(out, "\"%s\": ", key);
+    json_value_str(val, out, level + 2);
   }
 
-  fprintf(out, "\n");
+  g_string_append_c(out, '\n');
 
   space(out, level);
 
-  fprintf(out, "}");
+  g_string_append_c(out, '}');
 }
 
-static void json_print_array(struct json_object *obj, FILE *out, int level)
+static void json_array_str(struct json_object *obj, GString *out, int level)
 {
   int i;
   struct json_object *val;
   int first = 1;
 
-  fprintf(out, "[\n");
+  g_string_append(out, "[\n");
 
   for(i = 0; i < json_object_array_length(obj); i++) {
     if (!first) {
-      fprintf(out, ",\n");
+      g_string_append(out, ",\n");
     } else
       first = 0;
 
     space(out, level + 2);
 
     val = json_object_array_get_idx(obj, i);
-    json_print_value(val, out, level + 2);
+    json_value_str(val, out, level + 2);
   }
 
-  fprintf(out, "\n");
+  g_string_append_c(out, '\n');
 
   space(out, level);
 
-  fprintf(out, "]");
+  g_string_append_c(out, ']');
 }
 
-static void json_print_value(struct json_object *obj, FILE *out, int level)
+static void json_value_str(struct json_object *obj, GString *out, int level)
 {
   int i;
 
   switch(json_object_get_type(obj)) {
   case json_type_null:
-    fprintf(out, "(null)");
+    g_string_append(out, "(null)");
     break;
   case json_type_boolean:
-    fprintf(out, "%s", json_object_get_boolean(obj) ? "true" : "false");
+    g_string_append(out, json_object_get_boolean(obj) ? "true" : "false");
     break;
   case json_type_double:
-    fprintf(out, "%f", json_object_get_double(obj));
+    g_string_append_printf(out, "%f", json_object_get_double(obj));
     break;
   case json_type_int:
-    fprintf(out, "%d", json_object_get_int(obj));
+    g_string_append_printf(out, "%d", json_object_get_int(obj));
     break;
   case json_type_string:
-    fprintf(out, "\"%s\"", json_object_get_string(obj));
+    g_string_append_printf(out, "\"%s\"", json_object_get_string(obj));
     break;
   case json_type_object:
-    json_print_object(obj, out, level);
+    json_object_str(obj, out, level);
     break;
   case json_type_array:
-    json_print_array(obj, out, level);
+    json_array_str(obj, out, level);
   }
 }
 
-void uhuru_json_print(struct json_object *obj, FILE *out)
+const char *uhuru_json_str(struct json_object *obj)
 {
-  json_print_value(obj, out, 0);
-  fprintf(out, "\n");
+  GString *out = g_string_sized_new(100);
+  const char *ret;
+
+  json_value_str(obj, out, 0);
+
+  ret = out->str;
+
+  g_string_free(out, FALSE);
+
+  return ret;
 }
