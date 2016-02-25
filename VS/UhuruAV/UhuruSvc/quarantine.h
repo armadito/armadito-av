e40b0c1729c91f9a341cc72a5098504a311b6523
Ulrich <u.fausther@uhuru-solutions.com>
2016-02-25 10:02:08 +0100

    Uhuru-av-Windows :: restore quarantine file to previous location + others.c (containing utils functions)

--- VS/UhuruAV/UhuruSvc/quarantine.h
+++ VS/UhuruAV/UhuruSvc/quarantine.h
@@ -2,7 +2,10 @@
 #include <Windows.h>
 #include <string.h>
 #include <libuhuru\core.h>
+#include <json.h>
 
 #define QUARANTINE_DIR "C:\\Program Files\\Novit\\UhuruAV\\Quarantine"
+#define _QUARANTINE_DIR "Quarantine"
 
 int MoveFileInQuarantine(char * filepath);
+int RestoreFileFromQuarantine(char* filename);
