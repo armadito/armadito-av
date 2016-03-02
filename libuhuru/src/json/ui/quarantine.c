012da67abc90b97ffd1b3f6936eac6fe4e42b8ae
Ulrich <u.fausther@uhuru-solutions.com>
2016-03-02 11:47:44 +0100

    Uhuru-av-Windows :: quarantine builtin module

---
+++ libuhuru/src/json/ui/quarantine.c
@@ -0,0 +1,8 @@
+#include "quarantine.h"
+
+enum uhuru_json_status quarantine_response_cb(struct uhuru *uhuru, struct json_request *req, struct json_response *resp, void **request_data) {
+
+	// TO IMPLEMENT
+	return JSON_UNEXPECTED_ERR;
+
+}
