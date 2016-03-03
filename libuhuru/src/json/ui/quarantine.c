6947712c96034421cead5aaa8210e35282606c4d
François Déchelle <francois@dechelle.net>
2016-03-03 14:16:41 +0100

    fixed json stuff compilation on linux

--- libuhuru/src/json/ui/quarantine.c
+++ libuhuru/src/json/ui/quarantine.c
@@ -1,3 +1,7 @@
+#include "libuhuru-config.h"
+#include <libuhuru/core.h>
+
+#include "jsonhandler.h"
 #include "quarantine.h"
 
 enum uhuru_json_status quarantine_response_cb(struct uhuru *uhuru, struct json_request *req, struct json_response *resp, void **request_data) {
