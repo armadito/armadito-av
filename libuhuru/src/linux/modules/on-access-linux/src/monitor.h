79f44feee470784b4f8d0e70e8a6e6e836af4e5d
François Déchelle <francois@dechelle.net>
2016-03-03 14:09:45 +0100

    fixed callback execution in threads; added assert() to check that callbacks are called in the right thread

--- libuhuru/src/linux/modules/on-access-linux/src/monitor.h
+++ libuhuru/src/linux/modules/on-access-linux/src/monitor.h
@@ -1,6 +1,8 @@
 #ifndef _MONITOR_H_
 #define _MONITOR_H_
 
+#include <glib.h>
+
 struct access_monitor;
 
 struct access_monitor *access_monitor_new(struct uhuru *uhuru);
@@ -16,6 +18,8 @@ int access_monitor_is_enable_permission(struct access_monitor *m);
 int access_monitor_enable_removable_media(struct access_monitor *m, int enable_removable_media);
 
 int access_monitor_is_enable_removable_media(struct access_monitor *m);
+
+GMainContext *access_monitor_get_main_context(struct access_monitor *m);
 
 void access_monitor_add_mount(struct access_monitor *m, const char *mount_point);
 
