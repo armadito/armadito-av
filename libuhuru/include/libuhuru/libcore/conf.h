fc7863824ea75756c24fd80d7dbd086ecf9b7580
François Déchelle <francois@dechelle.net>
2016-03-10 14:42:21 +0100

    new conf in progress

--- libuhuru/include/libuhuru/libcore/conf.h
+++ libuhuru/include/libuhuru/libcore/conf.h
@@ -43,11 +43,21 @@ extern "C" {
 
   const char **uhuru_conf_get_keys(struct uhuru_conf *conf, const char *section, size_t *length);
 
-  const char *uhuru_conf_get_value(struct uhuru_conf *conf, const char *section, const char *key);
+  int uhuru_conf_is_int(struct uhuru_conf *conf, const char *section, const char *key);
+
+  int uhuru_conf_is_tring(struct uhuru_conf *conf, const char *section, const char *key);
+
+  int uhuru_conf_is_list(struct uhuru_conf *conf, const char *section, const char *key);
+
+  int uhuru_conf_get_uint(struct uhuru_conf *conf, const char *section, const char *key);
+
+  const char *uhuru_conf_get_string(struct uhuru_conf *conf, const char *section, const char *key);
 
   const char **uhuru_conf_get_list(struct uhuru_conf *conf, const char *section, const char *key, size_t *length);
 
-  void uhuru_conf_set_value(struct uhuru_conf *conf, const char *section, const char *key, const char *value);
+  void uhuru_conf_set_uint(struct uhuru_conf *conf, const char *section, const char *key, unsigned int value);
+
+  void uhuru_conf_set_string(struct uhuru_conf *conf, const char *section, const char *key, const char *value);
 
   void uhuru_conf_set_list(struct uhuru_conf *conf, const char *section, const char *key, const char **list, size_t length);
 
