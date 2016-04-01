5f2fe4ff8551b8b7d07df604fd8e8b7b1552eb7b
François Déchelle <fdechelle@teclib.com>
2016-04-01 10:35:08 +0200

    fixed scanning (on-demand & on-access) % changes in mime-types handling; tested for on-demand

--- libuhuru/src/linux/modules/on-access-linux/src/onaccessmod.c
+++ libuhuru/src/linux/modules/on-access-linux/src/onaccessmod.c
@@ -103,25 +103,37 @@ static enum uhuru_mod_status mod_oal_conf_white_list_dir(struct uhuru_module *mo
   return UHURU_MOD_OK;
 }
 
-#if 0
-static enum uhuru_mod_status mod_oal_conf_mime_type(struct uhuru_module *module, const char *directive, const char **argv)
+static enum uhuru_mod_status mod_oal_conf_modules(struct uhuru_module *module, const char *key, struct uhuru_conf_value *value)
 {
-  const char *mime_type;
   struct uhuru_scan_conf *on_access_conf = uhuru_scan_conf_on_access();
 
-  if (argv[0] == NULL || argv[1] == NULL) {
-    uhuru_log(UHURU_LOG_MODULE, UHURU_LOG_LEVEL_WARNING, MODULE_LOG_NAME ": " "invalid configuration directive, not enough arguments");
-    return UHURU_MOD_CONF_ERROR;
+  if (uhuru_conf_value_is_string(value))
+    uhuru_scan_conf_add_module(on_access_conf, uhuru_conf_value_get_string(value), module->uhuru);
+  else {
+    const char **p;
+
+    for (p = uhuru_conf_value_get_list(value); *p != NULL; p++)
+      uhuru_scan_conf_add_module(on_access_conf, *p, module->uhuru);
   }
-
-  mime_type = argv[0];
-
-  for(argv++; *argv != NULL; argv++)
-    uhuru_scan_conf_add_mime_type(on_access_conf, mime_type, *argv, module->uhuru);
 
   return UHURU_MOD_OK;
 }
-#endif
+
+static enum uhuru_mod_status mod_oal_conf_mime_types(struct uhuru_module *module, const char *key, struct uhuru_conf_value *value)
+{
+  struct uhuru_scan_conf *on_access_conf = uhuru_scan_conf_on_access();
+
+  if (uhuru_conf_value_is_string(value))
+    uhuru_scan_conf_add_mime_type(on_access_conf, uhuru_conf_value_get_string(value));
+  else {
+    const char **p;
+
+    for (p = uhuru_conf_value_get_list(value); *p != NULL; p++)
+      uhuru_scan_conf_add_mime_type(on_access_conf, *p);
+  }
+
+  return UHURU_MOD_OK;
+}
 
 static enum uhuru_mod_status mod_oal_conf_max_size(struct uhuru_module *module, const char *key, struct uhuru_conf_value *value)
 {
@@ -162,7 +174,8 @@ struct uhuru_conf_entry mod_oal_conf_table[] = {
   { "mount", CONF_TYPE_STRING | CONF_TYPE_LIST, mod_oal_conf_mount},
   { "directory", CONF_TYPE_STRING | CONF_TYPE_LIST, mod_oal_conf_directory},
   { "white-list-dir", CONF_TYPE_STRING | CONF_TYPE_LIST, mod_oal_conf_white_list_dir},
-  /* { "mime-type", mod_oal_conf_mime_type}, */
+  { "mime-types", CONF_TYPE_STRING | CONF_TYPE_LIST, mod_oal_conf_mime_types},
+  { "modules", CONF_TYPE_STRING | CONF_TYPE_LIST, mod_oal_conf_modules},
   { "max-size", CONF_TYPE_INT, mod_oal_conf_max_size},
   { NULL, 0, NULL},
 };
