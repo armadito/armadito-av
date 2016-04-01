56d2967a9d4140656ecb41521f303bb9958637da
François Déchelle <fdechelle@teclib.com>
2016-04-01 14:59:52 +0200

    reorganized libuhuru/include

---
+++ core/libarmadito/include/libarmadito.h
@@ -0,0 +1,19 @@
+#ifndef _LIBARMADITO_H_
+#define _LIBARMADITO_H_
+
+#include <libarmadito/action.h>
+#include <libarmadito/status.h>
+#include <libarmadito/error.h>
+#include <libarmadito/handle.h>
+#include <libarmadito/conf.h>
+#include <libarmadito/info.h>
+#include <libarmadito/module.h>
+#include <libarmadito/scanconf.h>
+#include <libarmadito/filectx.h>
+#include <libarmadito/scan.h>
+#include <libarmadito/ondemand.h>
+#include <libarmadito/log.h>
+#include <libarmadito/notify.h>
+#include <libarmadito/stdpaths.h>
+
+#endif
