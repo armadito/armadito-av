7213772c564f47c7ebc5d6e7bab7045279ac0e99
TounToun <v.hamon@uhuru-mobile.com>
2016-04-14 10:17:03 +0200

    Update scripts after reordering sources

--- scripts/do_all.sh
+++ scripts/do_all.sh
@@ -6,7 +6,6 @@ SRC_DIR=$DIR/../
 
 PACKAGE=$1
 
-
 ## Used to locally purge from all uhuru-av-packages
 if [[ $PACKAGE == "purge" ]]
 then
@@ -21,53 +20,60 @@ fi
 
 set -e
 
-if [[ $PACKAGE == "libuhuru" || $PACKAGE == "" ]];
+if [[ $PACKAGE == "core" || $PACKAGE == "" ]];
 then
-	./configure.sh -p libuhuru -d trusty
-	./compile.sh -p libuhuru -d trusty
-	./package.sh -p libuhuru -d trusty
+	./configure.sh -p core -d trusty
+	./compile.sh -p core -d trusty
+	#./package.sh -p core -d trusty
 fi
 
 if [[ $PACKAGE == "gui" || $PACKAGE == "" ]];
 then
 	./configure.sh -p gui -d trusty
 	./compile.sh -p gui -d trusty
-	./package.sh -p uhuru-qt -d trusty
+	#./package.sh -p uhuru-qt -d trusty
 fi
 
 if [[ $PACKAGE == "clamav" || $PACKAGE == "" ]];
 then
 	./configure.sh -p modules/clamav -d trusty
 	./compile.sh -p modules/clamav -d trusty
-	./package.sh -p uhuru-mod-clamav -d trusty
+	#./package.sh -p uhuru-mod-clamav -d trusty
 fi
 
 if [[ $PACKAGE == "module5_2" || $PACKAGE == "" ]];
 then
 	./configure.sh -p modules/module5_2 -d trusty
 	./compile.sh -p modules/module5_2 -d trusty
-	./package.sh -p uhuru-mod-module5-2 -d trusty
+	#./package.sh -p uhuru-mod-module5-2 -d trusty
 fi
 
 if [[ $PACKAGE == "fanotify" || $PACKAGE == "" ]];
 then
 	./configure.sh -p modules/fanotify -d trusty
 	./compile.sh -p modules/fanotify -d trusty
-	./package.sh -p uhuru-mod-fanotify -d trusty
+	#./package.sh -p uhuru-mod-fanotify -d trusty
 fi
 
-if [[ $PACKAGE == "desktop" || $PACKAGE == "" ]];
+if [[ $PACKAGE == "modulePDF" || $PACKAGE == "" ]];
 then
-	./package.sh -p uhuru-desktop -d trusty
+	./configure.sh -p modules/modulePDF -d trusty
+	./compile.sh -p modules/modulePDF -d trusty
+	#./package.sh -p uhuru-mod-fanotify -d trusty
 fi
 
-if [[ $PACKAGE == "cli" || $PACKAGE == "" ]];
-then
-	./package.sh -p uhuru-cli -d trusty
-fi
+#if [[ $PACKAGE == "desktop" || $PACKAGE == "" ]];
+#then
+#	./package.sh -p uhuru-desktop -d trusty
+#fi
 
-if [[ $PACKAGE == "" ]];
-then
-	./update_repo.sh
-fi
+#if [[ $PACKAGE == "cli" || $PACKAGE == "" ]];
+#then
+#	./package.sh -p uhuru-cli -d trusty
+#fi
+
+#if [[ $PACKAGE == "" ]];
+#then
+#	./update_repo.sh
+#fi
 
