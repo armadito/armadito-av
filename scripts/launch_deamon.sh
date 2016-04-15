15c8f72d8c42745093c97904cfcb7e90827bf203
TounToun <v.hamon@uhuru-mobile.com>
2016-04-15 12:06:53 +0200

    Update launching daemon scripts

--- scripts/launch_deamon.sh
+++ scripts/launch_deamon.sh
@@ -5,9 +5,11 @@ OS_V=ubuntu-14.04-64
 OUT_DIR=$DIR/../out
 SRC_DIR=$DIR/../
 
+
 set -e
+
 PREFIX=$OUT_DIR/install/$OS_V/armadito-av
-G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug --ipc=json
+#G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug --ipc=json
 
 # -- using valgrind
-# G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig valgrind -leak-check=full $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug --ipc=json 
+G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig valgrind --leak-check=full --show-leak-kinds=definite --log-file="a6o_leaks.log" $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug --ipc=json 
