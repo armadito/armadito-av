3455d87431ed8e87540b27415fc12af6fe95dbde
TounToun <v.hamon@uhuru-mobile.com>
2016-04-18 11:36:23 +0200

    comment free scan_conf after on_demand scan, add linux/update.c to makefile

--- scripts/launch_deamon.sh
+++ scripts/launch_deamon.sh
@@ -9,7 +9,7 @@ SRC_DIR=$DIR/../
 set -e
 
 PREFIX=$OUT_DIR/install/$OS_V/armadito-av
-#G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug --ipc=json
+G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug --ipc=json
 
 # -- using valgrind
-G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig valgrind --leak-check=full --show-leak-kinds=definite --log-file="a6o_leaks.log" $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug --ipc=json 
+#G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig valgrind --leak-check=full --show-leak-kinds=definite --log-file="a6o_leaks.log" $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug --ipc=json 
