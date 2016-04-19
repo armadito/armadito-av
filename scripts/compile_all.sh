91caa2c22295bd24b3e57afe4d13c8159394ab4a
TounToun <v.hamon@uhuru-mobile.com>
2016-04-19 11:39:52 +0200

    Update compilation scripts for easier use

--- scripts/compile_all.sh
+++ scripts/compile_all.sh
@@ -1,5 +1,7 @@
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# Modify version here
 OS_V=ubuntu-14.04-64
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 OUT_DIR=$DIR/../out
 SRC_DIR=$DIR/../
 
@@ -9,24 +11,24 @@ set -e
 
 if [[ $PACKAGE == "core" || $PACKAGE == "" ]];
 then
-	./configure.sh -p core -d trusty
-	./compile.sh -p core -d trusty
+	./configure.sh -p core -o $OS_V
+	./compile.sh -p core -o $OS_V
 fi
 
 if [[ $PACKAGE == "clamav" || $PACKAGE == "" ]];
 then
-	./configure.sh -p modules/clamav -d trusty
-	./compile.sh -p modules/clamav -d trusty
+	./configure.sh -p modules/clamav -o $OS_V
+	./compile.sh -p modules/clamav -o $OS_V
 fi
 
 if [[ $PACKAGE == "module5_2" || $PACKAGE == "" ]];
 then
-	./configure.sh -p modules/module5_2 -d trusty
-	./compile.sh -p modules/module5_2 -d trusty
+	./configure.sh -p modules/module5_2 -o $OS_V
+	./compile.sh -p modules/module5_2 -o $OS_V
 fi
 
 #if [[ $PACKAGE == "modulePDF" || $PACKAGE == "" ]];
 #then
-	#./configure.sh -p modules/modulePDF -d trusty
-#	./compile.sh -p modules/modulePDF -d trusty
+	#./configure.sh -p modules/modulePDF -o $OS_V
+#	./compile.sh -p modules/modulePDF -o $OS_V
 #fi
