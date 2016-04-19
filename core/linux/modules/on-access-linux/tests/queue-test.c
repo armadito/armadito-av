29b48a12257aa3e6f1b4f07dd1c0f0e659b39077
François Déchelle <fdechelle@teclib.com>
2016-04-19 16:34:50 +0200

    remaining renaming complete

--- core/linux/modules/on-access-linux/tests/queue-test.c
+++ core/linux/modules/on-access-linux/tests/queue-test.c
@@ -9,27 +9,27 @@
 
 static void queue_test_print(struct queue *q)
 {
-  struct queue_node *e;
+	struct queue_node *e;
 
-  printf("-> ");
+	printf("-> ");
 
-  e = q->head; 
-  while (e != NULL) {
-    printf("{%d, %ld.%09ld s} ", e->entry.fd, e->entry.timestamp.tv_sec, e->entry.timestamp.tv_nsec);
-    e = e->next;
-  }
+	e = q->head;
+	while (e != NULL) {
+		printf("{%d, %ld.%09ld s} ", e->entry.fd, e->entry.timestamp.tv_sec, e->entry.timestamp.tv_nsec);
+		e = e->next;
+	}
 
-  printf("\n");
+	printf("\n");
 
-  printf("<- ");
+	printf("<- ");
 
-  e = q->tail; 
-  while (e != NULL) {
-    printf("{%d, %ld.%09ld s} ", e->entry.fd, e->entry.timestamp.tv_sec, e->entry.timestamp.tv_nsec);
-    e = e->prev;
-  }
+	e = q->tail;
+	while (e != NULL) {
+		printf("{%d, %ld.%09ld s} ", e->entry.fd, e->entry.timestamp.tv_sec, e->entry.timestamp.tv_nsec);
+		e = e->prev;
+	}
 
-  printf("\n");
+	printf("\n");
 }
 
 static struct timespec zero_s_5 = { 0L, 500000000L};
@@ -38,50 +38,50 @@ static struct timespec one_s = { 1L, 0L};
 
 static void queue_test_push(struct queue *q)
 {
-  struct timespec now;
+	struct timespec now;
 
-  stamp_now(&now);
-  queue_push(q, 1, &now);
-  queue_push(q, 2, &now);
+	stamp_now(&now);
+	queue_push(q, 1, &now);
+	queue_push(q, 2, &now);
 
-  stamp_add(&now, &zero_s_5);
-  queue_push(q, 3, &now);
-  queue_push(q, 4, &now);
+	stamp_add(&now, &zero_s_5);
+	queue_push(q, 3, &now);
+	queue_push(q, 4, &now);
 
-  stamp_add(&now, &zero_s_7);
-  queue_push(q, 5, &now);
-  queue_push(q, 6, &now);
+	stamp_add(&now, &zero_s_7);
+	queue_push(q, 5, &now);
+	queue_push(q, 6, &now);
 }
 
 static void queue_test_timeout_1(struct queue *q)
 {
-  struct timespec now;
-  struct queue_entry entries[6];
-  int n;
+	struct timespec now;
+	struct queue_entry entries[6];
+	int n;
 
-  stamp_now(&now);
-  stamp_add(&now, &one_s);
+	stamp_now(&now);
+	stamp_add(&now, &one_s);
 
-  n = queue_pop_timeout(q, &now, entries, 6);
+	n = queue_pop_timeout(q, &now, entries, 6);
 
-  printf("pop'ed %d filedesc\n", n);
+	printf("pop'ed %d filedesc\n", n);
 }
 
 static void queue_test_1(void)
 {
-  struct queue *q;
+	struct queue *q;
 
-  q = queue_new();
+	q = queue_new();
 
-  queue_test_push(q);
+	queue_test_push(q);
 
-  queue_test_print(q);
+	queue_test_print(q);
 
-  queue_test_timeout_1(q);
+	queue_test_timeout_1(q);
 
-  queue_test_print(q);
+	queue_test_print(q);
 
-  queue_free(q);
+	queue_free(q);
 }
 
 #define PUSH_COUNT 50
@@ -89,44 +89,44 @@ static void queue_test_1(void)
 
 static void *push_thread_fun(void *arg)
 {
-  struct queue *q = (struct queue *)arg;
-  int i;
-  char statebuf[STATEBUFSZ];
-  struct random_data buf;
+	struct queue *q = (struct queue *)arg;
+	int i;
+	char statebuf[STATEBUFSZ];
+	struct random_data buf;
 
-  memset(statebuf, 0, STATEBUFSZ);
-  memset(&buf, 0, sizeof(buf));
+	memset(statebuf, 0, STATEBUFSZ);
+	memset(&buf, 0, sizeof(buf));
 	
-  if (initstate_r(0xdeadbeef, statebuf, STATEBUFSZ, &buf)) {
-    fprintf(stderr, "initstate_r failed (%s)\n", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
+	if (initstate_r(0xdeadbeef, statebuf, STATEBUFSZ, &buf)) {
+		fprintf(stderr, "initstate_r failed (%s)\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 
-  for(i = 0; i < PUSH_COUNT; i++) {
-    int fd, r;
-    struct timespec now;
-    struct timespec sleep_duration;
+	for(i = 0; i < PUSH_COUNT; i++) {
+		int fd, r;
+		struct timespec now;
+		struct timespec sleep_duration;
 
-    printf("=== push iteration %d\n", i);
-    stamp_now(&now);
+		printf("=== push iteration %d\n", i);
+		stamp_now(&now);
 
-    random_r(&buf, &fd);
-    fd %= 128;
+		random_r(&buf, &fd);
+		fd %= 128;
 
-    queue_push(q, fd, &now);
+		queue_push(q, fd, &now);
     
-    random_r(&buf, &r);
-    sleep_duration.tv_sec = 0;
-    sleep_duration.tv_nsec = (r % 3 + 1) * ONE_MILLISECOND;
+		random_r(&buf, &r);
+		sleep_duration.tv_sec = 0;
+		sleep_duration.tv_nsec = (r % 3 + 1) * ONE_MILLISECOND;
 
-    nanosleep(&sleep_duration, NULL);
-  }
+		nanosleep(&sleep_duration, NULL);
+	}
 
-  queue_test_print(q);
+	queue_test_print(q);
 
-  printf("push thread terminated\n");
+	printf("push thread terminated\n");
 
-  return NULL;
+	return NULL;
 }
 
 #define TIMEOUT_COUNT 500
@@ -134,62 +134,62 @@ static void *push_thread_fun(void *arg)
 
 static void *timeout_thread_fun(void *arg)
 {
-  struct queue *q = (struct queue *)arg;
-  struct timespec timeout = { 0, 200 * ONE_MICROSECOND};
-  struct timespec sleep_duration = { 0, 100 * ONE_MICROSECOND};
-  int i, n_fd;
-  struct timespec before;
-  long msec;
-  struct queue_entry entries[N_FD];
+	struct queue *q = (struct queue *)arg;
+	struct timespec timeout = { 0, 200 * ONE_MICROSECOND};
+	struct timespec sleep_duration = { 0, 100 * ONE_MICROSECOND};
+	int i, n_fd;
+	struct timespec before;
+	long msec;
+	struct queue_entry entries[N_FD];
 
-  for(i = 0; i < TIMEOUT_COUNT; i++) {
-    stamp_now(&before);
-    stamp_sub(&before, &timeout);
+	for(i = 0; i < TIMEOUT_COUNT; i++) {
+		stamp_now(&before);
+		stamp_sub(&before, &timeout);
 
-    n_fd = queue_pop_timeout(q, &before, entries, N_FD);
+		n_fd = queue_pop_timeout(q, &before, entries, N_FD);
 
-    if (n_fd)
-      printf("got %d fd in timeout\n", n_fd);
+		if (n_fd)
+			printf("got %d fd in timeout\n", n_fd);
 
-    nanosleep(&sleep_duration, NULL);
-  }
+		nanosleep(&sleep_duration, NULL);
+	}
 
-  printf("timeout thread terminated\n");
+	printf("timeout thread terminated\n");
 
-  return NULL;
+	return NULL;
 }
 
 static void queue_test_2(void)
 {
-  struct queue *q;
-  pthread_t push_thread, timeout_thread;
+	struct queue *q;
+	pthread_t push_thread, timeout_thread;
 
-  q = queue_new();
+	q = queue_new();
 
-  if (pthread_create(&push_thread, NULL, push_thread_fun, q)) {
-    fprintf(stderr, "pthread_create failed (%s)\n", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
+	if (pthread_create(&push_thread, NULL, push_thread_fun, q)) {
+		fprintf(stderr, "pthread_create failed (%s)\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 
-  if (pthread_create(&timeout_thread, NULL, timeout_thread_fun, q)) {
-    fprintf(stderr, "pthread_create failed (%s)\n", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
+	if (pthread_create(&timeout_thread, NULL, timeout_thread_fun, q)) {
+		fprintf(stderr, "pthread_create failed (%s)\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 
-  printf("joining push thread\n");
-  pthread_join(push_thread, NULL);
-  printf("push thread joined\n");
+	printf("joining push thread\n");
+	pthread_join(push_thread, NULL);
+	printf("push thread joined\n");
 
-  printf("joining timeout thread\n");
-  pthread_join(timeout_thread, NULL);
-  printf("timeout thread joined\n");
+	printf("joining timeout thread\n");
+	pthread_join(timeout_thread, NULL);
+	printf("timeout thread joined\n");
 
-  queue_free(q);
+	queue_free(q);
 }
 
 int main(int argc, char **argv)
 {
-  /* queue_test_1(); */
-  queue_test_2();
+	/* queue_test_1(); */
+	queue_test_2();
 }
 
