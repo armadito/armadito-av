bef9ffb94b151f981ea73c46066cd675168b9f85
François Déchelle <fdechelle@teclib.com>
2016-04-19 18:05:03 +0200

    debugging modules load on windows; changed log level in service

--- core/libarmadito/src/windows/os/dir.c
+++ core/libarmadito/src/windows/os/dir.c
@@ -14,24 +14,20 @@
 
 static enum os_file_flag dirent_flags(DWORD fileAttributes)
 {
-	switch(fileAttributes) {
+	/* (FD) fixed test which was not returning PLAIN_FILE for dlls */
+	if (fileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		return FILE_FLAG_IS_DIRECTORY;
+	if (fileAttributes & FILE_ATTRIBUTE_DEVICE)
+		return FILE_FLAG_IS_DEVICE;
+	if (fileAttributes & FILE_ATTRIBUTE_NORMAL
+		|| fileAttributes & FILE_ATTRIBUTE_ARCHIVE
+		|| fileAttributes & FILE_ATTRIBUTE_NOT_CONTENT_INDEXED)
+		return FILE_FLAG_IS_PLAIN_FILE;
 
-		case FILE_ATTRIBUTE_DIRECTORY:
-			return FILE_FLAG_IS_DIRECTORY;	  
-		case FILE_ATTRIBUTE_DEVICE:
-			return FILE_FLAG_IS_DEVICE;	  	  
-		//case DT_LNK:
-		//return FILE_FLAG_IS_LINK;
-		case FILE_ATTRIBUTE_NORMAL:
-			return FILE_FLAG_IS_PLAIN_FILE;
-		case FILE_ATTRIBUTE_ARCHIVE:
-			return FILE_FLAG_IS_PLAIN_FILE;
-		default:
-			return FILE_FLAG_IS_UNKNOWN;
-	}
+	return FILE_FLAG_IS_UNKNOWN;
 }
 
-BOOL DirectoryExists(LPCTSTR szPath)
+static BOOL DirectoryExists(LPCTSTR szPath)
 {
 	DWORD dwAttrib = GetFileAttributes(szPath);
 
@@ -42,47 +38,47 @@ BOOL DirectoryExists(LPCTSTR szPath)
 /*
   -- Note : Escaping not used anymore. But we need to be able to scan Chinese encoded name files
 
-void replaceAll(std::string& str, const std::string& from, const std::string& to) {
-	if (from.empty())
-		return;
-	size_t start_pos = 0;
-	while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
-		str.replace(start_pos, from.length(), to);
-		start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
-	}
-}
+  void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+  if (from.empty())
+  return;
+  size_t start_pos = 0;
+  while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+  str.replace(start_pos, from.length(), to);
+  start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+  }
+  }
 
-// We want escape ? and * characters 
-char *cpp_escape_str(const char * str_in){
+  // We want escape ? and * characters
+  char *cpp_escape_str(const char * str_in){
 
-	char * str_out = NULL;
-	std::string str(str_in);
+  char * str_out = NULL;
+  std::string str(str_in);
 
-	std::string c1("?");
-	std::string c2("*");
-	std::string c3("\\");
-	std::string c1_("\\?");
-	std::string c2_("\\*");
-	std::string c3_("\\\\");
+  std::string c1("?");
+  std::string c2("*");
+  std::string c3("\\");
+  std::string c1_("\\?");
+  std::string c2_("\\*");
+  std::string c3_("\\\\");
 
-	replaceAll(str, c3, c3_);
-	replaceAll(str, c1, c1_);
-	replaceAll(str, c2, c2_);
+  replaceAll(str, c3, c3_);
+  replaceAll(str, c1, c1_);
+  replaceAll(str, c2, c2_);
 
-	return _strdup(str.c_str());
-}
-*/
+  return _strdup(str.c_str());
+  }
+  */
 
 
-void os_dir_map(const char *path, int recurse, dirent_cb_t dirent_cb, void *data ) {
+void os_dir_map(const char *path, int recurse, dirent_cb_t dirent_cb, void *data) {
 
-	char * sPath = NULL, *entryPath= NULL;
+	char * sPath = NULL, *entryPath = NULL;
 	char * escapedPath = NULL;
 
 	HANDLE fh = NULL;
 	int size = 0;
-	WIN32_FIND_DATAA fdata ;
-	WIN32_FIND_DATAA tmp ;
+	WIN32_FIND_DATAA fdata;
+	WIN32_FIND_DATAA tmp;
 	enum os_file_flag flags;
 
 	// Check parameters
@@ -90,7 +86,7 @@ void os_dir_map(const char *path, int recurse, dirent_cb_t dirent_cb, void *data
 		a6o_log(ARMADITO_LOG_LIB, ARMADITO_LOG_LEVEL_WARNING, "Error :: NULL parameter in function os_dir_map()");
 		return;
 	}
-	
+
 	// We escape ? and * characters
 	// escapedPath = cpp_escape_str(path);
 
@@ -113,34 +109,27 @@ void os_dir_map(const char *path, int recurse, dirent_cb_t dirent_cb, void *data
 
 	fh = FindFirstFile(sPath, &fdata);
 	if (fh == INVALID_HANDLE_VALUE) {
-		a6o_log(ARMADITO_LOG_LIB, ARMADITO_LOG_LEVEL_WARNING, "Warning :: os_dir_map() :: FindFirstFileA() failed ::  (%s) :: [%s]", os_strerror(errno),sPath);
+		a6o_log(ARMADITO_LOG_LIB, ARMADITO_LOG_LEVEL_WARNING, "Warning :: os_dir_map() :: FindFirstFileA() failed ::  (%s) :: [%s]", os_strerror(errno), sPath);
 		return;
 	}
 
 	while (FindNextFile(fh, &tmp) != FALSE) {
-
-		//printf("[+] Debug :: os_dir_map :: entry name ===> [%s] <===\n",tmp.cFileName);
-
 		// exclude paths "." and ".."
-		if (strncmp(tmp.cFileName,".",strlen(tmp.cFileName)) == 0 || strncmp(tmp.cFileName,"..",strlen(tmp.cFileName)) == 0)
-		{
+		if (!strcmp(tmp.cFileName, ".") || !strcmp(tmp.cFileName, ".."))
 			continue;
-		}
 
 		// build the entry complete path.
-		size = strlen(path)+strlen(tmp.cFileName)+2;
+		size = strlen(path) + strlen(tmp.cFileName) + 2;
 
 		entryPath = (char*)calloc(size + 1, sizeof(char));
 		entryPath[size] = '\0';
-		sprintf_s(entryPath, size,"%s\\%s", path, tmp.cFileName);
-
+		sprintf_s(entryPath, size, "%s\\%s", path, tmp.cFileName);
 
 		// If it is a directory and we do recursive scan
 		if ((GetFileAttributesA(entryPath) & FILE_ATTRIBUTE_DIRECTORY) && recurse >= 1) {
 			os_dir_map(entryPath, recurse, dirent_cb, data);
 		}
 		else {
-			
 			flags = dirent_flags(tmp.dwFileAttributes);
 			(*dirent_cb)(entryPath, flags, 0, data);
 		}
@@ -152,7 +141,7 @@ void os_dir_map(const char *path, int recurse, dirent_cb_t dirent_cb, void *data
 		free(sPath);
 		sPath = NULL;
 	}
-	
+
 	FindClose(fh);
 
 	return;
@@ -167,12 +156,12 @@ void os_dir_map(const char *path, int recurse, dirent_cb_t dirent_cb, void *data
 #if 0
 static int stat_dir(const char *path)
 {
-  struct stat st;
+	struct stat st;
 
-  if (!stat(path, &st) && S_ISDIR(st.st_mode))
-    return 1;
+	if (!stat(path, &st) && S_ISDIR(st.st_mode))
+		return 1;
 
-  return (errno == ENOENT) ? 0 : -1;
+	return (errno == ENOENT) ? 0 : -1;
 }
 #endif
 
@@ -181,39 +170,3 @@ int os_mkdir_p(const char *path)
 	fprintf(stderr, "os_mkdir_p not implemented\n");
 	return -1;
 }
-
-
-char * GetBinaryDirectory( ) {
-
-	char * dirpath = NULL;
-	char filepath[MAX_PATH];
-	char * ptr = NULL;
-	int len = 0;
-
-	if (!GetModuleFileNameA(NULL, (LPSTR)&filepath, MAX_PATH)) {		
-		a6o_log(ARMADITO_LOG_LIB, ARMADITO_LOG_LEVEL_ERROR, "[-] Error :: GetBinaryDirectory!GetModuleFileName() failed :: %d\n",GetLastError());
-		return NULL;
-	}	
-
-	// get the file name from the complete file path
-	ptr = strrchr(filepath,'\\');
-	if (ptr == NULL) {		
-		a6o_log(ARMADITO_LOG_LIB, ARMADITO_LOG_LEVEL_WARNING, "[-] Error :: GetBinaryDirectory!strrchr() failed :: backslash not found in the path :: %s.\n",filepath);
-		return NULL;
-	}
-	
-	// calc the dir buffer length.
-	len = (int)(ptr - filepath);
-	//printf("[+] Debug :: GetBinaryDirectory :: ptr=%d :: filepath =%d :: len = %d :: strlen = %d\n",ptr,filepath,len,strlen(filepath));
-
-	dirpath = (char*)(calloc(len+1,sizeof(char)));
-	dirpath[len] = '\0';
-
-	memcpy_s(dirpath, len, filepath, len);
-
-	//uhuru_log(UHURU_LOG_LIB, UHURU_LOG_LEVEL_DEBUG, "[+] Debug :: GetBinaryDirectory :: dirpath = %s\n",dirpath);
-	
-
-	return dirpath;
-}
-
