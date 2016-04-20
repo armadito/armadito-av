0f7ce1a03e419076450948eccb8c8dc09f53ab29
Mohammed Zouggari <mzouggari@teclib.com>
2016-04-20 16:15:11 +0200

    Font awesome bug fixed (icons)

--- Gruntfile.js
+++ Gruntfile.js
@@ -392,6 +392,12 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        },
+        {
+          expand: true,
+          cwd: 'bower_components/font-awesome/',
+          src: 'fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
