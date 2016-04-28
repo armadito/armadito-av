c24c197544092c11f99166be4e5817393b0ea2be
TounToun <v.hamon@uhuru-mobile.com>
2016-04-28 12:50:08 +0200

    Cancel scan on exiting program by modal

--- app/scripts/controllers/ConfirmationController.js
+++ app/scripts/controllers/ConfirmationController.js
@@ -8,7 +8,34 @@
  * Controller of the tatouApp
  */
 angular.module('tatouApp')
-  .controller('ConfirmationController',['$scope', '$uibModalInstance', 'data', function ($scope, $uibModalInstance, data) {
+  .controller('ConfirmationController',['$scope', '$uibModalInstance', 'data', 'ArmaditoSVC', function ($scope, $uibModalInstance, data, ArmaditoSVC) {
+
+         $scope.threatDataFromAv = function(data){
+
+	      var json_object;
+	      var jobj_modules;
+	      var date = new Date().toISOString();
+
+	      try {
+
+		json_object = JSON.parse(data);
+
+		console.log("[+] Debug :: Data received from av :: ", json_object);
+
+		// Handle the cancel scan response of the av.
+		if(json_object.av_response == "scan_cancel" && json_object.status == 0 ){
+		  console.log("[+] Debug :: Scan successfully cancelled !\n");      
+		  global.scan_in_progress = 0;    
+		  return;
+		}
+
+	      }
+	      catch(e){
+		console.error("Parsing error:", e); 
+		return null;
+	      }
+
+	}
 
   	$scope.sentence = data.sentence;
   	$scope.title = data.title;
@@ -19,7 +46,11 @@ angular.module('tatouApp')
 
   	$scope.ok = function () {
 		$uibModalInstance.close($scope.quarantineLength);
-		// TODO: send cancel_scan to AV
+
+		// Send cancel_scan to AV if needed
+		if(global.scan_in_progress){ 
+		    ArmaditoSVC.cancelScan($scope.threatDataFromAv);
+                }
 	};
 
 	$scope.cancel = function () {
