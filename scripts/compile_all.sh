58b2267ad0dac141a790d26834e9f51334d1d6a9
TounToun <v.hamon@uhuru-mobile.com>
2016-05-02 16:36:39 +0200

    Add autoconf makefiles for modulePDF

--- scripts/compile_all.sh
+++ scripts/compile_all.sh
@@ -27,8 +27,8 @@ then
 	./compile.sh -p modules/module5_2 -o $OS_V
 fi
 
-#if [[ $PACKAGE == "modulePDF" || $PACKAGE == "" ]];
-#then
-	#./configure.sh -p modules/modulePDF -o $OS_V
-#	./compile.sh -p modules/modulePDF -o $OS_V
-#fi
+if [[ $PACKAGE == "modulePDF" || $PACKAGE == "" ]];
+then
+	./configure.sh -p modules/modulePDF -o $OS_V
+	./compile.sh -p modules/modulePDF -o $OS_V
+fi
