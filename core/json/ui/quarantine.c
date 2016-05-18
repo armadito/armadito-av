fd6a256023af85cd0e840f4013d951522ef66f38
Valentin Hamon <vhamon@teclib.com>
2016-05-18 15:09:52 +0200

    fix bad variable

--- core/json/ui/quarantine.c
+++ core/json/ui/quarantine.c
@@ -53,12 +53,12 @@ int process_info(const char * full_path,enum os_file_flag flags, int entry_errno
 	char * jfilecontent = NULL;
 	int len = 0;
 
-        if(strlen(str) < 5){
+        if(strlen(full_path) < 5){
 	   printf("[+] Debug :: Invalid full_path : %s\n",full_path);
 	   return 1;
 	}
 
-	if(strcmp(str + strlen(str) - 5, ".info") != 0)
+	if(strcmp(full_path + strlen(full_path) - 5, ".info") != 0)
 	  return 1;
 
 	// open the file.
