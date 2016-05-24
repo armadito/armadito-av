ddb1b9f84f2e801826c4b545af277ffc5b6c7dc0
TounToun <v.hamon@uhuru-mobile.com>
2016-05-24 15:06:03 +0200

    rename h1 in linux scripts

--- build/linux/scripts/internal_use_only/create_sym_links.sh
+++ build/linux/scripts/internal_use_only/create_sym_links.sh
@@ -9,4 +9,4 @@ PREFIX=$OUT_DIR/install/$OS_V/armadito-av
 
 mkdir -p $PREFIX/var/lib/armadito/bases/
 ln -s /var/lib/clamav $PREFIX/var/lib/armadito/bases/clamav
-ln -s /var/lib/uhuru/bases/module5_2/ $PREFIX/var/lib/armadito/bases/module5_2
+ln -s /var/lib/uhuru/bases/moduleH1/ $PREFIX/var/lib/armadito/bases/moduleH1
