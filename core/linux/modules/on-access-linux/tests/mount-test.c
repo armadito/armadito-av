b20bf59f55f82f65d42937417a4414de1ecd574c
TounToun <v.hamon@uhuru-mobile.com>
2016-05-25 14:17:24 +0200

    add LGPLv3 headers in core

--- core/linux/modules/on-access-linux/tests/mount-test.c
+++ core/linux/modules/on-access-linux/tests/mount-test.c
@@ -1,3 +1,24 @@
+/***
+
+Copyright (C) 2015, 2016, 2017 Teclib'
+
+This file is part of Armadito.
+
+Armadito is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Armadito is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Armadito.  If not, see <http://www.gnu.org/licenses/>.
+
+***/
+
 /* compile with:
    gcc $(pkg-config --cflags glib-2.0 gio-2.0 gio-unix-2.0) t.c mount.c -o mount-test $(pkg-config --libs glib-2.0 gio-2.0 gio-unix-2.0) -DDEBUG
 */
