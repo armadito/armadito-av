d750f2b42c5baff00178ecbb0993790553a84805
François Déchelle <fdechelle@teclib.com>
2016-06-07 15:14:08 +0200

    removed GPL header added by mistake

--- modules/moduleH1/src/lib/include/miniz.h
+++ modules/moduleH1/src/lib/include/miniz.h
@@ -1,24 +1,3 @@
-/***
-
-Copyright (C) 2015, 2016 Teclib'
-
-This file is part of Armadito module H1.
-
-Armadito module H1 is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Armadito module H1 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Armadito module H1.  If not, see <http://www.gnu.org/licenses/>.
-
-***/
-
 /* miniz.c v1.15 - public domain deflate/inflate, zlib-subset, ZIP reading/writing/appending, PNG writing
    See "unlicense" statement at the end of this file.
    Rich Geldreich <richgel99@gmail.com>, last updated Oct. 13, 2013
