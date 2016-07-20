dbd86113fddf1afbb1983490fed62d8a30c1d199
François Déchelle <fdechelle@teclib.com>
2016-07-20 14:18:36 +0200

    Move API implementation to its own file
    The API implementation is now in api.c

---
+++ core/linux/daemon/api.h
@@ -0,0 +1,43 @@
+/***
+
+Copyright (C) 2015, 2016 Teclib'
+
+This file is part of Armadito core.
+
+Armadito core is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Armadito core is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Armadito core.  If not, see <http://www.gnu.org/licenses/>.
+
+***/
+
+#ifndef DAEMON_API_H_
+#define DAEMON_API_H_
+
+#ifndef _WIN32
+#include <sys/select.h>
+#include <sys/socket.h>
+#else
+#include <winsock2.h>
+#endif
+#include <microhttpd.h>
+#include <json.h>
+#include <stdint.h>
+
+#include "httpd.h"
+
+struct a6o_api;
+
+int a6o_api_serve(struct a6o_api *a, struct MHD_Connection *connection, const char *path);
+
+
+
+#endif
