d908741752ec4fc5b3b524c77c8fc487bba6f12a
Valentin Hamon <vhamon@teclib.com>
2017-02-08 11:53:15 +0100

    Fix sprintf to snprintf more secure CWE-120

--- libcore/arch/linux/builtin-modules/on-access/famonitor.c
+++ libcore/arch/linux/builtin-modules/on-access/famonitor.c
@@ -120,7 +120,7 @@ static char *get_file_path_from_fd(int fd, char *buffer, size_t buffer_size)
 	if (fd <= 0)
 		return NULL;
 
-	sprintf(buffer, "/proc/self/fd/%d", fd);
+	snprintf(buffer, sizeof(buffer), "/proc/self/fd/%d", fd);
 	if ((len = readlink(buffer, buffer, buffer_size - 1)) < 0)
 		return NULL;
 
