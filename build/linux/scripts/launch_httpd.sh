8d408dc84e1252df7d37a50ff72980d0e9a35fa7
Valentin Hamon <vhamon@teclib.com>
2017-02-10 14:21:55 +0100

    Add script to launch httpd on linux

---
+++ build/linux/scripts/launch_httpd.sh
@@ -0,0 +1,11 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+OUT_DIR=$DIR/../out
+SRC_DIR=$DIR/../../../
+set -e
+
+PREFIX=$OUT_DIR/install/armadito-av
+G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig $PREFIX/sbin/armadito-httpd
+
+# -- using valgrind
+#G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig valgrind --leak-check=full --show-leak-kinds=definite --log-file="a6o_leaks.log" $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug
