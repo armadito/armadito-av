becb470f93168b85281ad391c078caa6c86e69cb
Valentin Hamon <vhamon@teclib.com>
2017-02-13 12:09:02 +0100

    Update linux binary exec scripts

--- build/linux/scripts/launch_httpd.sh
+++ build/linux/scripts/launch_httpd.sh
@@ -4,8 +4,17 @@ OUT_DIR=$DIR/../out
 SRC_DIR=$DIR/../../../
 set -e
 
+MODE=$1
 PREFIX=$OUT_DIR/install/armadito-av
-G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig $PREFIX/sbin/armadito-httpd
 
-# -- using valgrind
-#G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig valgrind --leak-check=full --show-leak-kinds=definite --log-file="a6o_leaks.log" $PREFIX/sbin/armadito-scand --no-daemon --log-level=debug
+if [[ $MODE == "gdb" ]];
+then
+        G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig libtool --mode=execute gdb $PREFIX/sbin/armadito-httpd
+fi
+
+if [[ $MODE == "valgrind" ]];
+then
+        G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig valgrind --leak-check=full --show-leak-kinds=definite --log-file="a6o_leaks.log" $PREFIX/sbin/armadito-httpd
+fi
+
+G_MESSAGES_DEBUG=all LD_LIBRARY_PATH=$PREFIX/lib PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig $PREFIX/sbin/armadito-httpd
