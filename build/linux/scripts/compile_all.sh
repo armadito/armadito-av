e4d2667e558e7878f0473d1f1441e1bd0c38c5fb
Maxime ROSSI BELLOM <mrossibellom@teclib.com>
2017-02-16 16:14:52 +0100

    Update projects path in compile script
    The tree structure generated by git-repo is different from the old one,
    so the compilation script has to be updated.

--- build/linux/scripts/compile_all.sh
+++ build/linux/scripts/compile_all.sh
@@ -1,57 +1,61 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 PACKAGE=$1
-REPO=$DIR/../../../../
+REPO=$DIR
+SCRIPTS=$REPO/core/build/linux/scripts
 
 # Modify git repositories' paths here
-CORE_SRC=$REPO/armadito-av
-CLAMAV_SRC=$REPO/armadito-mod-clamav
-H1_SRC=$REPO/armadito-mod-h1
-PDF_SRC=$REPO/armadito-mod-pdf
-WEBUI_SRC=$REPO/armadito-web-ui
-PRELUDE_SRC=$REPO/armadito-prelude/python
-SYSTRAY_SRC=$REPO/armadito-systray-ui/gtk
+CORE_SRC=$REPO/core
+CLAMAV_SRC=$REPO/modules/mod-clamav
+H1_SRC=$REPO/modules/mod-h1
+PDF_SRC=$REPO/modules/mod-pdf
+WEBUI_SRC=$REPO/ui/web-ui
+PRELUDE_SRC=$REPO/agents/prelude/python
+SYSTRAY_SRC=$REPO/ui/systray-ui/gtk
+
+CONFIGURESH=$SCRIPTS/configure.sh
+COMPILESH=$SCRIPTS/compile.sh
 
 set -e
 
 if [[ $PACKAGE == "core" || $PACKAGE == "" ]];
 then
-	./configure.sh -i $CORE_SRC -p core
-	./compile.sh -p core
+	. $CONFIGURESH -i $CORE_SRC -p core
+	. $COMPILESH -p core
 fi
 
 if [[ $PACKAGE == "clamav" || $PACKAGE == "" ]];
 then
-	./configure.sh -i $CLAMAV_SRC -p clamav
-	./compile.sh -p clamav
+	. $CONFIGURESH -i $CLAMAV_SRC -p clamav
+	. $COMPILESH -p clamav
 fi
 
 if [[ $PACKAGE == "moduleH1" || $PACKAGE == "" ]];
 then
-	./configure.sh -i $H1_SRC -p moduleH1
-	./compile.sh -p moduleH1
+	. $CONFIGURESH -i $H1_SRC -p moduleH1
+	. $COMPILESH -p moduleH1
 fi
 
 if [[ $PACKAGE == "modulePDF" || $PACKAGE == "" ]];
 then
-	./configure.sh -i $PDF_SRC -p modulePDF
-	./compile.sh -p modulePDF
+	. $CONFIGURESH -i $PDF_SRC -p modulePDF
+	. $COMPILESH -p modulePDF
 fi
 
 if [[ $PACKAGE == "python-prelude" ]];
 then
-	./configure.sh -i $PRELUDE_SRC -p prelude
-	./compile.sh -p prelude
+	. $CONFIGURESH -i $PRELUDE_SRC -p prelude
+	. $COMPILESH -p prelude
 fi
 
 if [[ $PACKAGE == "webui" || $PACKAGE == "" ]];
 then
-	./configure.sh -i $WEBUI_SRC -p webui
-	./compile.sh -p webui
+	. $CONFIGURESH -i $WEBUI_SRC -p webui
+	. $COMPILESH -p webui
 fi
 
 if [[ $PACKAGE == "systray" || $PACKAGE == "" ]];
 then
-	./configure.sh -i $SYSTRAY_SRC -p systray
-	./compile.sh -p systray
+	. $CONFIGURESH -i $SYSTRAY_SRC -p systray
+	. $COMPILESH -p systray
 fi
