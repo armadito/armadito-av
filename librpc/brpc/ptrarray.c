8f2af9e89f53f6764095936579d5a735b8cb7c86
François Déchelle <fdechelle@teclib.com>
2017-08-09 12:33:47 +0200

    Fix #include

--- librpc/brpc/ptrarray.c
+++ librpc/brpc/ptrarray.c
@@ -1,11 +1,10 @@
-#include "types.h"
 #include "buffer.h"
 #include "ptrarray.h"
 
 #include <stdlib.h>
 
 struct ptr_array {
-	struct buffer b;
+	struct buffer elements;
 	destroy_cb_t element_destroy_cb;
 };
 
@@ -15,7 +14,7 @@ struct ptr_array *ptr_array_new(destroy_cb_t element_destroy_cb)
 
 	array = malloc(sizeof(struct ptr_array));
 
-	buffer_init(&array->b, 0);
+	buffer_init(&array->elements, 0);
 	array->element_destroy_cb = element_destroy_cb;
 
 	return array;
@@ -23,35 +22,35 @@ struct ptr_array *ptr_array_new(destroy_cb_t element_destroy_cb)
 
 void ptr_array_free(struct ptr_array *array)
 {
-	buffer_destroy(&array->b, 1);
+	buffer_destroy(&array->elements, 1);
 	free(array);
 }
 
 void ptr_array_add(struct ptr_array *array, void *element)
 {
-	buffer_make_room(&array->b, sizeof(void *));
+	buffer_make_room(&array->elements, sizeof(void *));
 
-	*(void **)buffer_end(&array->b) = element;
+	*(void **)buffer_end(&array->elements) = element;
 
-	buffer_increment(&array->b, sizeof(void *));
+	buffer_increment(&array->elements, sizeof(void *));
 }
 
 size_t ptr_array_size(struct ptr_array *array)
 {
-	return buffer_size(&array->b) / sizeof(void *);
+	return buffer_size(&array->elements) / sizeof(void *);
 }
 
 void *ptr_array_index(struct ptr_array *array, int index)
 {
-	return *((void **)buffer_data(&array->b) + index);
+	return *((void **)buffer_data(&array->elements) + index);
 }
 
 void ptr_array_clear(struct ptr_array *array)
 {
 	void **p;
 
-	for(p = buffer_data(&array->b); p < (void **)buffer_end(&array->b); p++)
+	for(p = buffer_data(&array->elements); p < (void **)buffer_end(&array->elements); p++)
 		(*(array->element_destroy_cb))(*p);
 
-	buffer_clear(&array->b);
+	buffer_clear(&array->elements);
 }
