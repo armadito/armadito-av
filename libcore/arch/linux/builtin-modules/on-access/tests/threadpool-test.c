8604dd12190b43f759a25bee0c419a49bc96a852
François Déchelle <fdechelle@teclib.com>
2017-09-22 14:54:54 +0200

    Add thread pool test 2 using pipes

--- libcore/arch/linux/builtin-modules/on-access/tests/threadpool-test.c
+++ libcore/arch/linux/builtin-modules/on-access/tests/threadpool-test.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <pthread.h>
 
-static void *block(void *pool_data)
+static void *block_1(void *pool_data)
 {
 	unsigned int seconds = random() % 2 + 1;
 
@@ -17,7 +17,7 @@ static void *block(void *pool_data)
 	return malloc(1);
 }
 
-static int process(void *pool_data, void *data)
+static int process_1(void *pool_data, void *data)
 {
 	printf("thread 0x%lx is processing\n", pthread_self());
 
@@ -30,14 +30,94 @@ static int test_1(void)
 {
 	struct thread_pool *tp;
 
-	tp = thread_pool_new(8, block, process, NULL);
+	tp = thread_pool_new(8, block_1, process_1, NULL);
 
 	sleep(60);
 
 	return 0;
 }
 
+static void *block_2(void *pool_data)
+{
+	int fd = *(int *)pool_data;
+	char *p = malloc(1);
+
+	if (read(fd, p, 1) <= 0) {
+		perror("read");
+		free(p);
+		return NULL;
+	}
+
+	return p;
+}
+
+static int process_2(void *pool_data, void *data)
+{
+	char *p = data;
+
+	printf("thread 0x%lx has received %c\n", pthread_self(), *p);
+
+	free(p);
+
+	sleep(1);
+
+	return 0;
+}
+
+static int test_2(void)
+{
+	int command_pipe[2];
+	struct thread_pool *tp;
+	int *pool_data;
+
+	if (pipe(command_pipe) < 0) {
+		perror("pipe failed");
+		return 1;
+	}
+
+	pool_data = malloc(sizeof(int));
+	*pool_data = command_pipe[0];
+
+	tp = thread_pool_new(4, block_2, process_2, pool_data);
+
+	while(1) {
+		char c = 'A' + (random() % 26);
+
+		if (write(command_pipe[1], &c, 1) < 0) {
+			perror("write failed");
+			return 1;
+		}
+
+		sleep(1);
+	}
+
+	return 0;
+}
+
+static void usage(const char *progname)
+{
+	fprintf(stderr, "Usage: %s TEST_NUMBER\n", progname);
+	fprintf(stderr, "  TEST_NUMBER: 1,2\n");
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char **argv)
 {
-	return test_1();
+	int which_test;
+
+	if (argc < 2)
+		usage(argv[0]);
+	if (sscanf(argv[1], "%d", &which_test) != 1)
+		usage(argv[0]);
+
+	switch(which_test) {
+	case 1:
+		return test_1();
+	case 2:
+		return test_2();
+	default:
+		usage(argv[0]);
+	}
+
+	return 0;
 }
