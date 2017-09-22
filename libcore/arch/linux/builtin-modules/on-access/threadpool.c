b62135d7e814774f3cf42c3038a80f94bc4fc9f9
François Déchelle <fdechelle@teclib.com>
2017-09-22 11:49:28 +0200

    Add thread & mutex initialization

--- libcore/arch/linux/builtin-modules/on-access/threadpool.c
+++ libcore/arch/linux/builtin-modules/on-access/threadpool.c
@@ -9,21 +9,9 @@ struct thread_pool {
 	process_fun_t process_fun;
 	void *pool_data;
 	int n_threads;
-	pthread_mutex_t *mutex;
+	pthread_t *threads;
+	pthread_mutex_t mutex;
 };
-
-struct thread_pool *thread_pool_new(int n_threads, blocking_fun_t bf, process_fun_t pf, void *pool_data)
-{
-	struct thread_pool *tp = malloc(sizeof(struct thread_pool));
-
-	tp->blocking_fun = bf;
-	tp->process_fun = pf;
-	tp->pool_data = pool_data;
-
-	/* create the threads */
-
-	return tp;
-}
 
 static void *thread_fun(void *arg)
 {
@@ -31,9 +19,9 @@ static void *thread_fun(void *arg)
 	struct thread_pool *pool = (struct thread_pool *)arg;
 
 	while (1) {
-		pthread_mutex_lock(pool->mutex);
+		pthread_mutex_lock(&pool->mutex);
 		data = (*pool->blocking_fun)(pool->pool_data);
-		pthread_mutex_unlock(pool->mutex);
+		pthread_mutex_unlock(&pool->mutex);
 
 		if (data == NULL)
 			return NULL;
@@ -42,6 +30,35 @@ static void *thread_fun(void *arg)
 			return NULL;
 	}
 
+	return NULL;
+}
+
+struct thread_pool *thread_pool_new(int n_threads, blocking_fun_t bf, process_fun_t pf, void *pool_data)
+{
+	struct thread_pool *tp = malloc(sizeof(struct thread_pool));
+	int n;
+
+	tp->blocking_fun = bf;
+	tp->process_fun = pf;
+	tp->pool_data = pool_data;
+	tp->threads = calloc(n_threads, sizeof(pthread_t));
+
+	pthread_mutex_init(&tp->mutex, NULL);
+
+	/* create the threads */
+	for (n = 0; n < n_threads; n++) {
+		if (pthread_create(&tp->threads[n], NULL, thread_fun, tp))
+			goto ret_err;
+	}
+
+	return tp;
+
+ret_err:
+	pthread_mutex_destroy(&tp->mutex);
+
+	/* must deinitialize the threads */
+
+	free(tp);
 	return NULL;
 }
 
